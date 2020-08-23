ecca53bb5b8257deb38f68f7dd18a3d3c8acd9c4
Cedric Hippmann <cedric.hippmann@hotmail.com>
2020-08-23 16:36:58 +0200

    Change string creation

--- src/jni/jvm_loader.cpp
+++ src/jni/jvm_loader.cpp
@@ -78,21 +78,21 @@ jni::GetCreatedJavaVMs jni::JvmLoader::getGetCreatedJavaVMsFunction() {
 
 const char *jni::JvmLoader::getJvmLibPath() {
 #ifdef __linux__
-    String relativePath = "lib/server/libjvm.so";
-    String fileSeparator = "/";
+    String relativePath = {"lib/server/libjvm.so"};
+    String fileSeparator = {"/"};
 #elif TARGET_OS_MAC
-    String relativePath = "lib/server/libjvm.dylib";
-    String fileSeparator = "/";
+    String relativePath = {"lib/server/libjvm.dylib"};
+    String fileSeparator = {"/"};
 #elif defined _WIN32 || defined _WIN64
-    String relativePath = "bin\\server\\jvm.dll";
-    String fileSeparator = "\\";
+    String relativePath = {"bin\\server\\jvm.dll"};
+    String fileSeparator = {"\\"};
 #endif
 
     String embeddedJrePath = "jre/";
 
     if (Engine::get_singleton()->is_editor_hint()) {
 
-        String message = "No embedded jvm found on path: jre" + fileSeparator + relativePath + "!" ;
+        String message = "No embedded jvm found on path: jre" + fileSeparator + relativePath + "!";
         String javaHome = getenv("JAVA_HOME");
 
         if (javaHome.empty()) {
