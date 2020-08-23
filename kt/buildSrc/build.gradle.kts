3435f8a222023f1f231238bc3ede73c6e3ece550
Pierre-Thomas Meisels <meisels27@yahoo.fr>
2020-08-23 16:06:23 +0200

    add base code of api generation as submodule

--- kt/buildSrc/build.gradle.kts
+++ kt/buildSrc/build.gradle.kts
@@ -8,4 +8,6 @@ repositories {
 
 dependencies {
     implementation(kotlin("gradle-plugin", version = "1.4.0"))
+    implementation("com.squareup:kotlinpoet:1.5.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.11.0")
 }
