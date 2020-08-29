2197c66e316df5c6400b5b07d8869d49a80ef438
Pierre-Thomas Meisels <meisels27@yahoo.fr>
2020-08-29 14:10:47 +0200

    Add PropertyInfo

--- harness/tests/src/main/kotlin/godot/tests/Entry.kt
+++ harness/tests/src/main/kotlin/godot/tests/Entry.kt
@@ -1,21 +1,66 @@
 package godot.tests
 
-import godot.core.KtConstructor0
-import godot.core.KtFunction1
-import godot.core.KtFunctionInfo
-import godot.core.KtVariant
+import godot.core.*
 
 class Entry : godot.runtime.Entry() {
     override fun Context.init() {
         with(registry) {
             registerClass<Invocation>(Invocation::class.qualifiedName!!, "Spatial") {
                 constructor(KtConstructor0(::Invocation))
-                function(KtFunction1(KtFunctionInfo("int", ::KtVariant), Invocation::int, KtVariant::asInt))
-                function(KtFunction1(KtFunctionInfo("long", ::KtVariant), Invocation::long, KtVariant::asLong))
-                function(KtFunction1(KtFunctionInfo("float", ::KtVariant), Invocation::float, KtVariant::asFloat))
-                function(KtFunction1(KtFunctionInfo("double", ::KtVariant), Invocation::double, KtVariant::asDouble))
-                function(KtFunction1(KtFunctionInfo("boolean", ::KtVariant), Invocation::boolean, KtVariant::asBoolean))
-                function(KtFunction1(KtFunctionInfo("string", ::KtVariant), Invocation::string, KtVariant::asString))
+				function(KtFunction1(KtFunctionInfo("int", listOf(
+						KtPropertyInfo(
+								KtVariant.Type.LONG,
+								"value",
+								"Int",
+								PropertyHint(),
+								""
+						)
+				)), Invocation::int, ::KtVariant, KtVariant::asInt))
+                function(KtFunction1(KtFunctionInfo("long", listOf(
+						KtPropertyInfo(
+								KtVariant.Type.LONG,
+								"value",
+								"Long",
+								PropertyHint(),
+								""
+						)
+				)), Invocation::long, ::KtVariant, KtVariant::asLong))
+                function(KtFunction1(KtFunctionInfo("float", listOf(
+						KtPropertyInfo(
+								KtVariant.Type.DOUBLE,
+								"value",
+								"Float",
+								PropertyHint(),
+								""
+						)
+				)), Invocation::float, ::KtVariant, KtVariant::asFloat))
+                function(KtFunction1(KtFunctionInfo("double", listOf(
+						KtPropertyInfo(
+								KtVariant.Type.DOUBLE,
+								"value",
+								"Double",
+								PropertyHint(),
+								""
+						)
+				)), Invocation::double, ::KtVariant, KtVariant::asDouble))
+                function(KtFunction1(KtFunctionInfo("boolean", listOf(
+						KtPropertyInfo(
+								KtVariant.Type.BOOL,
+								"value",
+								"Boolean",
+								PropertyHint(),
+								""
+						)
+				)), Invocation::boolean, ::KtVariant, KtVariant::asBoolean))
+                function(KtFunction1(KtFunctionInfo("string", listOf(
+						KtPropertyInfo(
+								KtVariant.Type.STRING,
+								"value",
+								"String",
+								PropertyHint(),
+								""
+						)
+				)), Invocation::string, ::KtVariant, KtVariant::asString))
             }
         }
     }
--- kt/godot-runtime/src/main/kotlin/godot/core/Functions.kt
+++ kt/godot-runtime/src/main/kotlin/godot/core/Functions.kt
@@ -2,13 +2,23 @@ package godot.core
 
 import godot.util.camelToSnakeCase
 
-data class KtFunctionInfo<T: KtObject, R>(
+class PropertyHint
+
+data class KtPropertyInfo (
+		val type: KtVariant.Type,
 		val name: String,
-		val returnValueConverter: (R) -> KtVariant,
+		val className: String,
+		val hint: PropertyHint,
+		val hintString: String
+)
+
+data class KtFunctionInfo (
+		val name: String,
+		val propertyInfos: List<KtPropertyInfo>
 )
 
 abstract class KtFunction<T: KtObject, R>(
-		val functionInfo: KtFunctionInfo<T, R>,
+		val functionInfo: KtFunctionInfo,
 		val parameterCount: Int
 ) {
     val registrationName = functionInfo.name.camelToSnakeCase()
@@ -23,24 +33,26 @@ abstract class KtFunction<T: KtObject, R>(
 }
 
 class KtFunction0<T: KtObject, R>(
-		functionInfo: KtFunctionInfo<T, R>,
-		private val function: (T) -> R
+		functionInfo: KtFunctionInfo,
+		private val function: (T) -> R,
+		private val returnValueConverter: (R) -> KtVariant
 ) : KtFunction<T, R>(functionInfo, 0) {
     override operator fun invoke(instance: T, args: List<KtVariant>): KtVariant {
-        return functionInfo.returnValueConverter(
+        return returnValueConverter(
             function(instance)
         )
     }
 }
 
 class KtFunction1<T: KtObject, P0, R>(
-		functionInfo: KtFunctionInfo<T, R>,
+		functionInfo: KtFunctionInfo,
 		private val function: (T, P0) -> R,
+		private val returnValueConverter: (R) -> KtVariant,
 		private val p0Converter: (KtVariant) -> P0
 ) : KtFunction<T, R>(functionInfo, 1) {
     override operator fun invoke(instance: T, args: List<KtVariant>): KtVariant {
         require(args.size == parameterCount) { "Expecting $parameterCount parameter(s), but got ${args.size} instead." }
-        return functionInfo.returnValueConverter(
+        return returnValueConverter(
 			function(
                 instance,
                 p0Converter(args[0])
@@ -50,13 +62,14 @@ class KtFunction1<T: KtObject, P0, R>(
 }
 
 class KtFunction2<T: KtObject, P0, P1, R>(
-    functionInfo: KtFunctionInfo<T, R>,
+    functionInfo: KtFunctionInfo,
     private val function: (T, P0, P1) -> R,
+    private val returnValueConverter: (R) -> KtVariant,
     private val p0Converter: (KtVariant) -> P0,
     private val p1Converter: (KtVariant) -> P1,
 ) : KtFunction<T, R>(functionInfo, 1) {
     override fun invoke(instance: T, args: List<KtVariant>): KtVariant {
-        return functionInfo.returnValueConverter(
+        return returnValueConverter(
             function(
                 instance,
                 p0Converter(args[0]),
@@ -67,14 +80,15 @@ class KtFunction2<T: KtObject, P0, P1, R>(
 }
 
 class KtFunction3<T: KtObject, P0, P1, P2, R>(
-    functionInfo: KtFunctionInfo<T, R>,
+    functionInfo: KtFunctionInfo,
     private val function: (T, P0, P1, P2) -> R,
+    private val returnValueConverter: (R) -> KtVariant,
     private val p0Converter: (KtVariant) -> P0,
     private val p1Converter: (KtVariant) -> P1,
     private val p2Converter: (KtVariant) -> P2,
 ) : KtFunction<T, R>(functionInfo, 1) {
     override fun invoke(instance: T, args: List<KtVariant>): KtVariant {
-        return functionInfo.returnValueConverter(
+        return returnValueConverter(
             function(
                 instance,
                 p0Converter(args[0]),
@@ -86,15 +100,16 @@ class KtFunction3<T: KtObject, P0, P1, P2, R>(
 }
 
 class KtFunction4<T: KtObject, P0, P1, P2, P3, R>(
-    functionInfo: KtFunctionInfo<T, R>,
+    functionInfo: KtFunctionInfo,
     private val function: (T, P0, P1, P2, P3) -> R,
+    private val returnValueConverter: (R) -> KtVariant,
     private val p0Converter: (KtVariant) -> P0,
     private val p1Converter: (KtVariant) -> P1,
     private val p2Converter: (KtVariant) -> P2,
     private val p3Converter: (KtVariant) -> P3,
 ) : KtFunction<T, R>(functionInfo, 1) {
     override fun invoke(instance: T, args: List<KtVariant>): KtVariant {
-        return functionInfo.returnValueConverter(
+        return returnValueConverter(
             function(
                 instance,
                 p0Converter(args[0]),
@@ -107,8 +122,9 @@ class KtFunction4<T: KtObject, P0, P1, P2, P3, R>(
 }
 
 class KtFunction5<T: KtObject, P0, P1, P2, P3, P4, R>(
-    functionInfo: KtFunctionInfo<T, R>,
+    functionInfo: KtFunctionInfo,
     private val function: (T, P0, P1, P2, P3, P4) -> R,
+    private val returnValueConverter: (R) -> KtVariant,
     private val p0Converter: (KtVariant) -> P0,
     private val p1Converter: (KtVariant) -> P1,
     private val p2Converter: (KtVariant) -> P2,
@@ -116,7 +132,7 @@ class KtFunction5<T: KtObject, P0, P1, P2, P3, P4, R>(
     private val p4Converter: (KtVariant) -> P4,
 ) : KtFunction<T, R>(functionInfo, 1) {
     override fun invoke(instance: T, args: List<KtVariant>): KtVariant {
-        return functionInfo.returnValueConverter(
+        return returnValueConverter(
             function(
                 instance,
                 p0Converter(args[0]),
