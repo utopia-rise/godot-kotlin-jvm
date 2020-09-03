2af5c77603335ac16b7d484462f56934310e330c
Pierre-Thomas Meisels <meisels27@yahoo.fr>
2020-09-03 13:56:52 +0200

    reformat Invocation

--- harness/tests/src/main/kotlin/godot/tests/Invocation.kt
+++ harness/tests/src/main/kotlin/godot/tests/Invocation.kt
@@ -3,10 +3,10 @@ package godot.tests
 import godot.Object
 
 class Invocation : Object() {
-	fun int(value: Int) = value
-	fun long(value: Long) = value
-	fun float(value: Float) = value
-	fun double(value: Double) = value
-	fun boolean(value: Boolean) = value
-	fun string(value: String) = value
+    fun int(value: Int) = value
+    fun long(value: Long) = value
+    fun float(value: Float) = value
+    fun double(value: Double) = value
+    fun boolean(value: Boolean) = value
+    fun string(value: String) = value
 }
