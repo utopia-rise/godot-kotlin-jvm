390b7b29ec428b1cde204db523b1a5a6edce7146
Pierre-Thomas Meisels <48322356+piiertho@users.noreply.github.com>
2020-09-09 12:56:59 +0200

    Add registration of KtClass properties (#7)

---
+++ src/kt_property.cpp
@@ -0,0 +1,49 @@
+#include <modules/kotlin_jvm/src/wire/wire.pb.h>
+#include "kt_property.h"
+#include "kt_variant.h"
+#include "gd_kotlin.h"
+
+KtPropertyInfo::KtPropertyInfo(jni::JObject p_wrapped, jni::JObject& p_class_loader)
+        : JavaInstanceWrapper("godot.core.KtPropertyInfo", p_wrapped, p_class_loader) {
+    jni::Env env { jni::Jvm::current_env() };
+    jni::MethodId getTypeMethod{get_method_id(env, "getType", "()I")};
+    type = KtVariant::fromWireType(static_cast<wire::Value::TypeCase>(wrapped.call_int_method(env, getTypeMethod)));
+    jni::MethodId getNameMethod{get_method_id(env, "getName", "()Ljava/lang/String;")};
+    name = env.from_jstring(wrapped.call_object_method(env, getNameMethod));
+    jni::MethodId getClassNameMethod{get_method_id(env, "getClassName", "()Ljava/lang/String;")};
+    class_name = env.from_jstring(wrapped.call_object_method(env, getClassNameMethod));
+    jni::MethodId getPropertyHintMethod{get_method_id(env, "getHint", "()I")};
+    hint = static_cast<PropertyHint>(wrapped.call_int_method(env, getPropertyHintMethod));
+    jni::MethodId getHintStringMethod{get_method_id(env, "getHintString", "()Ljava/lang/String;")};
+    hint_string = env.from_jstring(wrapped.call_object_method(env, getHintStringMethod));
+}
+
+PropertyInfo KtPropertyInfo::toPropertyInfo() {
+    PropertyInfo info;
+    info.type = type;
+    info.name = name;
+    info.class_name = class_name;
+    info.hint = hint;
+    info.hint_string = hint_string;
+    return info;
+}
+
+KtProperty::KtProperty(jni::JObject p_wrapped, jni::JObject& p_class_loader)
+        : JavaInstanceWrapper("godot.core.KtProperty", p_wrapped, p_class_loader) {
+    jni::Env env { jni::Jvm::current_env() };
+    jni::MethodId getKtPropertyInfoMethod{get_method_id(env, "getKtPropertyInfo", "()Lgodot/core/KtPropertyInfo;")};
+    propertyInfo = new KtPropertyInfo(wrapped.call_object_method(env, getKtPropertyInfoMethod),
+                                      GDKotlin::get_instance().get_class_loader());
+}
+
+KtProperty::~KtProperty() {
+    delete propertyInfo;
+}
+
+StringName KtProperty::get_name() const {
+    return propertyInfo->name;
+}
+
+PropertyInfo KtProperty::get_member_info() {
+    return propertyInfo->toPropertyInfo();
+}
