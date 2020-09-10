bb4283a33b92b6f20b85ec4e3da9bcc7ff60387c
Pierre-Thomas Meisels <meisels27@yahoo.fr>
2020-09-10 19:34:06 +0200

    Fixed test to avoid orphans

--- harness/tests/test/unit/test_calls.gd
+++ harness/tests/test/unit/test_calls.gd
@@ -1,13 +1,7 @@
 extends "res://addons/gut/test.gd"
 
-var invocation_scene = load("res://Spatial.tscn").instance()
-
-func before_each():
-	invocation_scene = load("res://Spatial.tscn").instance()
-
-func after_each():
-	print("free invocation")
-	invocation_scene.free()
 
 func test_godot_to_jvm_call_using_scene() -> void:
+	var invocation_scene = load("res://Spatial.tscn").instance()
 	assert_eq(invocation_scene.intAddition(2, 7), 9, "call to addition jvm method should return 2 + 7")
+	invocation_scene.free()
