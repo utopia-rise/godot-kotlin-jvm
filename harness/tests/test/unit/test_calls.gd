980276d3fb89d4e838afd1b4f9be48d69321ca61
Pierre-Thomas Meisels <meisels27@yahoo.fr>
2020-09-11 13:58:23 +0200

    rename jvm cast methods in Invocation

--- harness/tests/test/unit/test_calls.gd
+++ harness/tests/test/unit/test_calls.gd
@@ -5,3 +5,13 @@ func test_godot_to_jvm_call_using_scene() -> void:
 	var invocation_scene = load("res://Spatial.tscn").instance()
 	assert_eq(invocation_scene.intAddition(2, 7), 9, "call to addition jvm method should return 2 + 7")
 	invocation_scene.free()
+
+func test_should_not_call_method_with_too_much_parameter() -> void:
+	var invocation_scene = load("res://Spatial.tscn").instance()
+	assert_ne(invocation_scene.intAddition(2, 7, 1), 9, "Method should not be called with too much parameter")
+	invocation_scene.free()
+
+func test_int_value() -> void:
+	var invocation_scene = load("res://Spatial.tscn").instance()
+	assert_eq(invocation_scene.intValue(2, 3), 2, "call to addition jvm method should return 2 + 7")
+	invocation_scene.free()
