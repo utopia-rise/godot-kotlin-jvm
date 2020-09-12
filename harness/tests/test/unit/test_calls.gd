899d4fe13378f7f55312479e261e375bf66f6cdc
Pierre-Thomas Meisels <meisels27@yahoo.fr>
2020-09-12 11:30:00 +0200

    Add tests on calling method with kotlin script instantiated from gdscript

--- harness/tests/test/unit/test_calls.gd
+++ harness/tests/test/unit/test_calls.gd
@@ -6,6 +6,11 @@ func test_godot_to_jvm_call_using_scene() -> void:
 	assert_eq(invocation_scene.int_addition(2, 7), 9, "call to addition jvm method should return 2 + 7")
 	invocation_scene.free()
 
+func test_godot_to_jvm_call_using_new() -> void:
+	var invocation_script = load("res://src/main/kotlin/godot/tests/Invocation.kt").new()
+	assert_eq(invocation_script.int_addition(2, 7), 9, "call to addition jvm method should return 2 + 7")
+	invocation_script.free()
+
 func test_int_value() -> void:
 	var invocation_scene = load("res://Spatial.tscn").instance()
 	assert_eq(invocation_scene.int_value(2), 2, "int value cast should return exactly same value")
