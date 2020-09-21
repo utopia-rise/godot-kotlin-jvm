34299a42b4c53e345f27a7618eccbcc3e53f4dc5
Pierre-Thomas Meisels <meisels27@yahoo.fr>
2020-09-21 17:44:04 +0200

    Use api-generator as included build plugin

--- kt/godot-library/build.gradle.kts
+++ kt/godot-library/build.gradle.kts
@@ -1,5 +1,13 @@
 plugins {
     kotlin("jvm")
+    id("com.utopia-rise.api-generator")
+}
+
+apply<godot.gradle.ApiGeneratorPlugin>()
+
+configure<godot.gradle.ApiGeneratorPluginExtension> {
+    outputDir.set(project.file("$rootDir/godot-library/src/main/kotlin/godot/gen"))
+    sourceJson.set(project.file("$rootDir/api-generator/src/main/resources/api.json"))
 }
 
 dependencies {
