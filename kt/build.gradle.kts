e2fdac462585c6e2ad1094ef876b9c34377f7d1d
Pierre-Thomas Meisels <meisels27@yahoo.fr>
2020-09-21 17:44:04 +0200

    Use api-generator as included build plugin

--- kt/build.gradle.kts
+++ kt/build.gradle.kts
@@ -1,6 +1,16 @@
 version = "0.1.0-${DependenciesVersions.godotVersion}"
 val versionString = project.version.toString()
 
+buildscript {
+    repositories {
+        jcenter()
+        gradlePluginPortal()
+    }
+    dependencies {
+        "classpath"(group = "com.utopia-rise", name = "api-generator", version = "0.0.1")
+    }
+}
+
 subprojects {
     group = "com.utopia-rise"
     version = versionString
