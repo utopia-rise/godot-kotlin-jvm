0d3de58c2dff7fad3ff5fcb23b4f24407c240588
Pierre-Thomas Meisels <meisels27@yahoo.fr>
2020-10-01 13:51:01 +0200

    Add engine types to TypeManager

--- kt/godot-runtime/src/main/kotlin/godot/core/TypeManager.kt
+++ kt/godot-runtime/src/main/kotlin/godot/core/TypeManager.kt
@@ -2,12 +2,15 @@ package godot.core
 
 object TypeManager {
     private val userTypes = HashSet<String>()
+    private val engineTypes = HashMap<String, () -> KtObject>()
 
-    fun registerUserType(className: String) {
-        userTypes.add(className)
+    fun registerUserType(className: String) = userTypes.add(className)
+
+    fun registerEngineType(className: String, invocator: () -> KtObject) {
+        engineTypes["className"] = invocator
     }
 
-    fun isUserType(className: String): Boolean {
-        return userTypes.contains(className)
-    }
+    fun isUserType(className: String) = userTypes.contains(className)
+
+    fun isEngineType(className: String) = engineTypes.contains(className)
 }
