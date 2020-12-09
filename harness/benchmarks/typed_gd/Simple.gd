a9c181242ebe172d9bd0a2fcbc8d3610e3132e28
dev003 <dev003@utopia-rise.com>
2020-12-09 10:02:15 +0100

    Forgot to commit the typed script ><

--- harness/benchmarks/typed_gd/Simple.gd
+++ harness/benchmarks/typed_gd/Simple.gd
@@ -1,51 +1,51 @@
-func benchmark_simple_add():
-	var a = 1
-	var b = 2
+func benchmark_simple_add() -> int:
+	var a: int  = 1
+	var b: int = 2
 	return a + b
 
-func benchmark_avg():
-	var size = 10000
-	var total = 0
+func benchmark_avg() -> int:
+	var size: int = 10000
+	var total: int = 0
 	for i in range(size):
 		total += i
 	return total / size
 
-func benchmark_vectors():
-	var b = Transform()
+func benchmark_vectors() -> Vector3:
+	var b: Transform = Transform()
 	b = b.rotated(Vector3.UP, deg2rad(60))
 	b = b.scaled(Vector3(0.5, 0.5, 0.5))
 
 	var s: Vector3 = Vector3()
 	for i in range(1000):
-		var v  = Vector3(i, i, i)
+		var v: Vector3 = Vector3(i, i, i)
 		v = b.xform(v)
 		s = s + v
 
 	return s
 
-func benchmark_vectors2_only():
-	var s = Vector2()
+func benchmark_vectors2_only() -> Vector2:
+	var s: Vector2 = Vector2()
 	for i in range(1000):
 		var v = Vector2(i, i)
 		s = s + v
 
 	return s
 
-func benchmark_icall():
+func benchmark_icall() -> void:
 	var node: Node = Node.new()
 	node.get_instance_id()
 	node.free()
 	return
 
-func benchmark_icall_with_loop():
-	var node = Node.new()
+func benchmark_icall_with_loop() -> void:
+	var node: Node = Node.new()
 	for i in range(100):
-		var child = Node.new()
+		var child: Node = Node.new()
 		node.add_child(child)
 		node.remove_child(child)
 		child.free()
 	node.free()
 	return
 
-func benchmark_method_call():
+func benchmark_method_call() -> void:
 	pass
