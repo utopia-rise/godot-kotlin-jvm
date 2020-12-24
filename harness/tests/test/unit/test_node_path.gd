3b467a6b41b9c460a44f1105c780bdbfb446fbc4
Pierre-Thomas Meisels <meisels27@yahoo.fr>
2020-12-24 14:12:47 +0100

    Fix typo in test_node_path

--- harness/tests/test/unit/test_node_path.gd
+++ harness/tests/test/unit/test_node_path.gd
@@ -3,7 +3,7 @@ extends "res://addons/gut/test.gd"
 
 func test_should_find_camera_node():
 	var invocation_scene = load("res://Spatial.tscn").instance()
-	assert_true(invocation_scene.has_camera_node(), "Should find Camera Node using not path")
+	assert_true(invocation_scene.has_camera_node(), "Should find Camera Node using node path")
 	invocation_scene.free()
 
 func test_should_not_find_camera_node():
