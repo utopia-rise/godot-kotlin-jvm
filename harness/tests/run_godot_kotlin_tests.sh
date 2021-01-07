2cc5dce2c52a0aa90e7514f2247d47e5a81d3ad2
Pierre-Thomas Meisels <meisels27@yahoo.fr>
2021-01-07 20:36:45 +0100

    add run tests script as cannot create organization gist :/

---
+++ harness/tests/run_godot_kotlin_tests.sh
@@ -0,0 +1,21 @@
+$1 -d -s --path $PWD addons/gut/gut_cmdln.gd | ( no_error="false"; jvm_closed="false"; while read -r line
+do
+    echo "$line"
+    if echo "$line" | grep -q "Failing asserts:  0"; then
+        no_error="true"
+    fi
+    if echo "$line" | grep -q "Shutting down JVM ..."; then
+        jvm_closed="true"
+    fi
+done
+if [[ "$no_error" == "false" ]]; then
+    echo "ERROR: Some assertions failed !"
+    exit 1
+elif [[ "$jvm_closed" == "false" ]]; then
+    echo "ERROR: JVM has not closed properly !"
+    exit 1
+else
+    echo "ERROR: Some assertions failed !"
+    exit 0
+fi
+); exit $?
