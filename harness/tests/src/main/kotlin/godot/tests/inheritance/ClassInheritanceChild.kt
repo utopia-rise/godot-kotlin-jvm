65eea51f32809d5a91304b58995f28d5435250a5
Cedric Hippmann <cedric.hippmann@hotmail.com>
2021-01-15 12:56:18 +0100

    Update entry gen ref to include support for script inheritance and add some more example cases to test inherited classes

--- harness/tests/src/main/kotlin/godot/tests/inheritance/ClassInheritanceChild.kt
+++ harness/tests/src/main/kotlin/godot/tests/inheritance/ClassInheritanceChild.kt
@@ -1,10 +1,16 @@
 package godot.tests.inheritance
 
 import godot.annotation.RegisterClass
+import godot.annotation.RegisterFunction
 import godot.annotation.RegisterProperty
+import godot.annotation.RegisterSignal
+import godot.signals.signal
 
 @RegisterClass
 class ClassInheritanceChild : ClassInheritanceParent() {
+
+    @RegisterSignal
+    override val signalTestOverridden by signal<String, Int>("blubb", "habbalubb")
 
     //---------------- Here to check ------------------
 
@@ -15,6 +21,7 @@ class ClassInheritanceChild : ClassInheritanceParent() {
 
     override var openVar: Int = 100
 
+    @RegisterFunction
     override fun openFunction() {
         childOpenFunctionHasBeenCalled = true
     }
--- harness/tests/src/main/kotlin/godot/tests/inheritance/ClassInheritanceParent.kt
+++ harness/tests/src/main/kotlin/godot/tests/inheritance/ClassInheritanceParent.kt
@@ -4,9 +4,17 @@ import godot.Node
 import godot.annotation.RegisterClass
 import godot.annotation.RegisterFunction
 import godot.annotation.RegisterProperty
+import godot.annotation.RegisterSignal
+import godot.signals.signal
 
 @RegisterClass
 open class ClassInheritanceParent : Node() {
+
+    @RegisterSignal
+    val signalTestNotOverridden by signal<String>("blubb")
+
+    @RegisterSignal
+    open val signalTestOverridden by signal<String, Int>("blubb", "habbalubb")
 
     //---------------- Here to check ------------------
 
