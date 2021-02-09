2abee5050fd7dc9fb38eacd76270ea932a3571d9
Cedric Hippmann <cedric.hippmann@hotmail.com>
2021-02-09 19:58:28 +0100

    Change comment to kdoc

--- kt/godot-runtime/src/main/kotlin/godot/util/Utils.kt
+++ kt/godot-runtime/src/main/kotlin/godot/util/Utils.kt
@@ -29,8 +29,10 @@ internal inline fun Number.toGodotReal(): Float = this.toFloat()
 @PublishedApi
 internal inline fun Float.toGodotReal(): Float = this
 
-//Because of float precision, it's hard to obtain two perfectly equal real numbers.
-// They are considered equal if the difference is smaller than CMP_EPSILON
+/**
+ * Because of float precision, it's hard to obtain two perfectly equal real numbers.
+ * They are considered equal if the difference is smaller than CMP_EPSILON
+ */
 const val CMP_EPSILON: RealT = 0.00001
 fun isEqualApprox(a: RealT, b: RealT, epsilon: RealT = CMP_EPSILON): Boolean {
     //taken from https://github.com/godotengine/godot/blob/d8066aa6a4afb12ffddcec71bd7e051dcd04f3e1/core/math/math_funcs.h#L315
