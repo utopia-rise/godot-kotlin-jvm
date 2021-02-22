f67459903d316b892bc6133be6f187f018e2f99e
Cedric Hippmann <cedric.hippmann@hotmail.com>
2021-02-22 18:52:14 +0100

    Support multi args constructors

--- harness/tests/src/main/kotlin/godot/tests/MultiArgsConstructorTest.kt
+++ harness/tests/src/main/kotlin/godot/tests/MultiArgsConstructorTest.kt
@@ -1,8 +1,11 @@
 package godot.tests
 
+import godot.NavigationMesh
 import godot.Node
+import godot.Object
 import godot.annotation.RegisterClass
 import godot.annotation.RegisterProperty
+import godot.core.VariantArray
 
 @RegisterClass
 class MultiArgsConstructorTest : Node {
@@ -20,11 +23,19 @@ class MultiArgsConstructorTest : Node {
         defaultConstructorHasBeenCalled = true
     }
 
-//    constructor(i: Int) : this() {
-//        oneArgConstructorHasBeenCalled = true
-//    }
-//
-//    constructor(i: Int, s: String) : this() {
-//        threeArgsConstructorHasBeenCalled = true
-//    }
+    constructor(i: Int) : this() {
+        oneArgConstructorHasBeenCalled = true
+    }
+
+    constructor(i: Int, s: String) : this() {
+        threeArgsConstructorHasBeenCalled = true
+    }
+
+    constructor(i: Int, s: String, obj: Object?) : this()
+    constructor(i: Int, s: String, obj: Object?, variantArray: VariantArray<Any?>) : this()
+    constructor(i: Int, s: String, obj: Object?, variantArray: VariantArray<Any?>?, navMesh: NavigationMesh) : this()
+
+    // constructors which should fail:
+//    constructor(iShouldFailAsOverloadingIsNotSupported: String, s: String, obj: Object?, variantArray: VariantArray<Any?>?, navMesh: NavigationMesh) : this()
+//    constructor(i: Int, s: String, obj: Object?, variantArray: VariantArray<Any?>?, navMesh: NavigationMesh, tooManyArgs: String) : this()
 }
