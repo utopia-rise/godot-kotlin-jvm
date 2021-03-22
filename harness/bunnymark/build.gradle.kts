896e6188e5ab4764728cb0784ad7f6fdd54258db
Cedric Hippmann <cedric.hippmann@hotmail.com>
2021-03-22 09:52:36 +0100

    Fix godot version

--- harness/bunnymark/build.gradle.kts
+++ harness/bunnymark/build.gradle.kts
@@ -1,7 +1,7 @@
 
 plugins {
     kotlin("jvm") version "1.4.10"
-    id("com.utopia-rise.godot-jvm") version "0.1.0-3.2"
+    id("com.utopia-rise.godot-jvm") version "0.1.0-3.2.3"
 }
 
 repositories {
