22410c7eb4ec5bfa3cbfad9c59fb659f59e65db6
Cedric Hippmann <cedric.hippmann@hotmail.com>
2021-03-26 09:33:52 +0100

    Use grgit and git tags to define version and releaseMode

--- kt/build.gradle.kts
+++ kt/build.gradle.kts
@@ -1,4 +1,21 @@
-version = "0.1.0-${DependenciesVersions.godotVersion}"
+import org.ajoberstar.grgit.Commit
+
+plugins {
+    id("org.ajoberstar.grgit") version "4.1.0"
+}
+
+val currentCommit: Commit = grgit.head()
+// check if the current commit is tagged
+var tagOnCurrentCommit = grgit.tag.list().firstOrNull { tag -> tag.commit.id == currentCommit.id }
+var releaseMode = tagOnCurrentCommit != null
+
+version = if (!releaseMode) {
+    "$godotKotlinJvmVersion-${currentCommit.abbreviatedId}-SNAPSHOT"
+} else {
+    val godotKotlinJvmTagVersion = requireNotNull(tagOnCurrentCommit).name
+    "$godotKotlinJvmTagVersion-${DependenciesVersions.godotVersion}"
+}
+
 val versionString = project.version.toString()
 
 subprojects {
---
+++ kt/buildSrc/src/main/kotlin/godotKotlinJvmVersion.kt
@@ -0,0 +1,1 @@
+const val godotKotlinJvmVersion = "0.1.0"
