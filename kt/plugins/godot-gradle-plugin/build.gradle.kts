be68bd1897fb6f0d54115bae0b2c6b2dcbea8e6b
Cedric Hippmann <cedric.hippmann@hotmail.com>
2021-04-13 15:33:06 +0200

    Update gradle publish plugin

--- kt/plugins/godot-gradle-plugin/build.gradle.kts
+++ kt/plugins/godot-gradle-plugin/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     `kotlin-dsl`
     `maven-publish`
-    id("com.gradle.plugin-publish") version "0.10.1"
+    id("com.gradle.plugin-publish") version "0.14.0"
     id("com.utopia-rise.godot-publish")
 }
 
