7d37608999a523b4b9c33f2e674166f43532d795
Cedric Hippmann <cedric.hippmann@hotmail.com>
2021-04-15 10:26:39 +0200

    Fix entry gen version and bump version codes

--- kt/buildSrc/src/main/kotlin/godotKotlinJvmVersion.kt
+++ kt/buildSrc/src/main/kotlin/godotKotlinJvmVersion.kt
@@ -1,2 +1,2 @@
-const val godotKotlinJvmVersion = "0.1.0"
-const val godotKotlinIntellijPluginVersion = "0.1.0"
+const val godotKotlinJvmVersion = "0.1.1"
+const val godotKotlinIntellijPluginVersion = "0.1.1"
