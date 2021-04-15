d6e13b5e02a75be900824e6f24740de79b874300
Cedric Hippmann <cedric.hippmann@hotmail.com>
2021-04-15 12:02:12 +0200

    Bump version code

--- kt/buildSrc/src/main/kotlin/godotKotlinJvmVersion.kt
+++ kt/buildSrc/src/main/kotlin/godotKotlinJvmVersion.kt
@@ -1,2 +1,2 @@
-const val godotKotlinJvmVersion = "0.1.1"
-const val godotKotlinIntellijPluginVersion = "0.1.1"
+const val godotKotlinJvmVersion = "0.1.2"
+const val godotKotlinIntellijPluginVersion = "0.1.2"
