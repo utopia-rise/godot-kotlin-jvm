f33a7bbbb31a8f2fbdeafb13016ad35836e7ba80
Cedric Hippmann <cedric.hippmann@hotmail.com>
2021-04-22 14:43:55 +0200

    Remove obsolete dependencies

--- kt/buildSrc/build.gradle.kts
+++ kt/buildSrc/build.gradle.kts
@@ -26,6 +26,4 @@ gradlePlugin {
 
 dependencies {
     implementation(kotlin("gradle-plugin", version = "1.4.32"))
-    implementation("com.squareup:kotlinpoet:1.5.0")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.11.0")
 }
