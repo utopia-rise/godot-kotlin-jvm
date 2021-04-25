2f96f1b8d29b9113c98c6fc8cd400ec6770aa610
Cedric Hippmann <cedric.hippmann@hotmail.com>
2021-04-25 14:05:17 +0200

    Fix submodule handling in module creation wizard

--- kt/plugins/godot-intellij-plugin/src/main/kotlin/godot/intellij/plugin/module/GodotModuleBuilder.kt
+++ kt/plugins/godot-intellij-plugin/src/main/kotlin/godot/intellij/plugin/module/GodotModuleBuilder.kt
@@ -5,6 +5,7 @@ import com.intellij.ide.util.projectWizard.ModuleBuilderListener
 import com.intellij.ide.util.projectWizard.ModuleWizardStep
 import com.intellij.ide.util.projectWizard.WizardContext
 import com.intellij.openapi.Disposable
+import com.intellij.openapi.externalSystem.model.project.dependencies.ProjectDependencies
 import com.intellij.openapi.fileChooser.FileChooserDescriptor
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.module.ModuleType
@@ -15,6 +16,7 @@ import com.intellij.ui.layout.GrowPolicy
 import com.intellij.ui.layout.panel
 import com.intellij.ui.layout.selected
 import godot.intellij.plugin.GodotPluginBundle
+import org.jetbrains.plugins.gradle.util.GradleUtil
 import java.io.File
 import javax.swing.JCheckBox
 import javax.swing.JComponent
@@ -72,9 +74,11 @@ class GodotModuleBuilder : ModuleBuilder(), ModuleBuilderListener {
                             versionTextField().growPolicy(GrowPolicy.MEDIUM_TEXT)
                         }
                     }
-                    titledRow(GodotPluginBundle.message("wizard.projectSettings.dependencySettings.title")) {
-                        row(GodotPluginBundle.message("wizard.projectSettings.dependencySettings.kotlinVersion")) {
-                            kotlinVersionTextField().growPolicy(GrowPolicy.MEDIUM_TEXT)
+                    if (wizardContext.isCreatingNewProject) {
+                        titledRow(GodotPluginBundle.message("wizard.projectSettings.dependencySettings.title")) {
+                            row(GodotPluginBundle.message("wizard.projectSettings.dependencySettings.kotlinVersion")) {
+                                kotlinVersionTextField().growPolicy(GrowPolicy.MEDIUM_TEXT)
+                            }
                         }
                     }
                     titledRow(GodotPluginBundle.message("wizard.projectSettings.buildSettings.title")) {
@@ -114,36 +118,86 @@ class GodotModuleBuilder : ModuleBuilder(), ModuleBuilderListener {
     }
 
     override fun moduleCreated(module: Module) {
-        val basePath = module.project.basePath ?: return
+        val basePath = (wizardContext.projectBuilder as? GodotModuleBuilder)?.contentEntryPath ?: return
         val group = wizardContext.getUserData(groupIdKey) ?: "com.example"
         val artifact = wizardContext.getUserData(artifactIdKey) ?: "game"
         val packagePathAsFolderStructure = "$group.$artifact".replace(".", "/")
 
         File(basePath, "src/main/kotlin/$packagePathAsFolderStructure").mkdirs()
 
-        copyTemplateFile(basePath, "gradle/wrapper/gradle-wrapper.jar")
-        copyTemplateFile(basePath, "gradle/wrapper/gradle-wrapper.properties")
-        copyTemplateFile(basePath, "gradlew")
-        copyTemplateFile(basePath, "gradlew.bat")
-        copyTemplateFile(basePath, "build.gradle.kts") { outFile ->
-            outFile.writeText(
-                outFile
-                    .readText()
-                    .replace("KOTLIN_VERSION", wizardContext.getUserData(kotlinVersionKey) ?: "1.4.32")
-                    .replace(
-                        "GODOT_KOTLIN_JVM_VERSION",
-                        "0.1.4-3.3.0"
-                    )
-                    .replace("ANDROID_ENABLED", wizardContext.getUserData(androidEnabledKey)?.toString() ?: "false")
-                    .replace("DX_TOOL_PATH", wizardContext.getUserData(dxToolPathKey) ?: "dx")
-            )
-        }
-        copyTemplateFile(basePath, "settings.gradle.kts") { outFile ->
-            outFile.writeText(
-                outFile
-                    .readText()
-                    .replace("PROJECT_NAME", module.project.name)
-            )
+        if (wizardContext.isCreatingNewProject) {
+            copyTemplateFile(basePath, "gradle/wrapper/gradle-wrapper.jar")
+            copyTemplateFile(basePath, "gradle/wrapper/gradle-wrapper.properties")
+            copyTemplateFile(basePath, "gradlew")
+            copyTemplateFile(basePath, "gradlew.bat")
+            copyTemplateFile(basePath, "build.gradle.kts") { outFile ->
+                outFile.writeText(
+                    outFile
+                        .readText()
+                        .replace("KOTLIN_VERSION", wizardContext.getUserData(kotlinVersionKey) ?: "1.4.32")
+                        .replace(
+                            "GODOT_KOTLIN_JVM_VERSION",
+                            "0.1.4-3.3.0"
+                        )
+                        .replace("ANDROID_ENABLED", wizardContext.getUserData(androidEnabledKey)?.toString() ?: "false")
+                        .replace("DX_TOOL_PATH", wizardContext.getUserData(dxToolPathKey) ?: "dx")
+                )
+            }
+            copyTemplateFile(basePath, "settings.gradle.kts") { outFile ->
+                outFile.writeText(
+                    outFile
+                        .readText()
+                        .replace("PROJECT_NAME", module.project.name)
+                )
+            }
+        } else {
+            val projectBasePath = module.project.basePath ?: return
+            File(projectBasePath, "settings.gradle.kts").also { settingsFile ->
+                if (settingsFile.exists()) {
+                    val moduleRelativePath = basePath.removePrefix(projectBasePath)
+                    val modulePath = moduleRelativePath
+                        .replace("/", ":")
+                        .replace(File.separator, ":")
+                    settingsFile.appendText("\ninclude(\"$modulePath\")")
+                }
+            }
+
+            copyTemplateFile(
+                basePath,
+                "build.gradle.kts.submodule",
+                "build.gradle.kts"
+            ) { outFile ->
+                outFile.writeText(
+                    outFile
+                        .readText()
+                        .replace("ANDROID_ENABLED", wizardContext.getUserData(androidEnabledKey)?.toString() ?: "false")
+                        .replace("DX_TOOL_PATH", wizardContext.getUserData(dxToolPathKey) ?: "dx")
+                        .let { content ->
+                            if (module.parentProjectAlreadyContainsDependency(wizardContext, "godot-library")) {
+                                content.replace(
+                                    "GODOT_KOTLIN_DEPENDENCY",
+                                    "id(\"com.utopia-rise.godot-kotlin-jvm\")"
+                                )
+                            } else {
+                                content.replace(
+                                    "GODOT_KOTLIN_DEPENDENCY",
+                                    "id(\"com.utopia-rise.godot-kotlin-jvm\") version \"0.1.4-3.2.3\""
+                                )
+                            }
+                            if (module.parentProjectAlreadyContainsDependency(wizardContext, "kotlin-stdlib")) {
+                                content.replace(
+                                    "KOTLIN_DEPENDENCY",
+                                    "kotlin(\"jvm\")"
+                                )
+                            } else {
+                                content.replace(
+                                    "KOTLIN_DEPENDENCY",
+                                    "kotlin(\"jvm\") version \"${wizardContext.getUserData(kotlinVersionKey) ?: "1.4.32"}\""
+                                )
+                            }
+                        }
+                )
+            }
         }
         copyTemplateFile(basePath, "src/main/kotlin/$packagePathAsFolderStructure/Simple.kt") { outFile ->
             outFile.writeText(
@@ -166,22 +220,47 @@ class GodotModuleBuilder : ModuleBuilder(), ModuleBuilderListener {
         copyTemplateFile(basePath, ".gitignore")
     }
 
-    private fun copyTemplateFile(baseDir: String, filePath: String, modifyOutFile: (File) -> Unit = {}) {
-        val fileName = filePath.substringAfterLast("/")
-        val parentDir = filePath.substringBeforeLast("/")
+    private fun copyTemplateFile(
+        baseDir: String,
+        sourceFileName: String,
+        targetFileName: String = sourceFileName.substringAfterLast("/"),
+        modifyOutFile: (File) -> Unit = {}
+    ) {
+        val templateFile = "${sourceFileName.substringAfterLast("/")}.template"
+        val parentDir = sourceFileName.substringBeforeLast("/")
 
-        val parentFile = if (parentDir.isNotEmpty() && parentDir != fileName) {
+        val parentFile = if (parentDir.isNotEmpty() && parentDir != sourceFileName) {
             File(baseDir, parentDir).apply {
                 mkdirs()
             }
         } else File(baseDir)
 
-        val outFile = File(parentFile, fileName)
+        val outFile = File(parentFile, targetFileName)
 
-        this::class.java.getResourceAsStream("/template/$fileName.template").use { inputStream ->
-            inputStream.copyTo(outFile.outputStream())
+        this::class.java.getResourceAsStream("/template/$templateFile").use { inputStream ->
+            inputStream?.copyTo(outFile.outputStream())
         }
 
         modifyOutFile(outFile)
+    }
+
+    private fun Module.parentProjectAlreadyContainsDependency(
+        wizardContext: WizardContext,
+        dependency: String
+    ): Boolean {
+        return if (!wizardContext.isCreatingNewProject) {
+            project.basePath?.let { projectBasePath ->
+                @Suppress("UnstableApiUsage")
+                GradleUtil
+                    .findGradleModuleData(project, projectBasePath)
+                    ?.children
+                    ?.filter { it.data is ProjectDependencies }
+                    ?.flatMap { (it.data as ProjectDependencies).componentsDependencies }
+                    ?.flatMap { it.compileDependenciesGraph.dependencies }
+                    ?.any { it.displayName.contains(dependency) }
+            } == true
+        } else {
+            false
+        }
     }
 }
