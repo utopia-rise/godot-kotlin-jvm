1037db6d2c0f3cb82583971d6e58b669dcfa7fef
Cedric Hippmann <cedric.hippmann@hotmail.com>
2021-06-23 18:39:16 +0200

    Readd godot configuration

--- harness/tests/build.gradle.kts
+++ harness/tests/build.gradle.kts
@@ -8,11 +8,11 @@ repositories {
     mavenCentral()
 }
 
-//godot {
-//    //uncomment to test android
-////    isAndroidExportEnabled.set(true)
-////    dxToolPath.set("${System.getenv("ANDROID_SDK_ROOT")}/build-tools/30.0.3/dx")
-//}
+godot {
+    //uncomment to test android
+//    isAndroidExportEnabled.set(true)
+//    dxToolPath.set("${System.getenv("ANDROID_SDK_ROOT")}/build-tools/30.0.3/dx")
+}
 
 dependencies {
     implementation("joda-time:joda-time:2.10.6") //external dependency to test dependency inclusion in dummyCompilation
