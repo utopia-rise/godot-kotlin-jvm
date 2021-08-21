2032602af389d433cf6ce45a305b9bf5b1e642f8
Cedric Hippmann <cedric.hippmann@hotmail.com>
2021-08-21 17:49:43 +0200

    Fix main shadowJar creation

--- kt/plugins/godot-gradle-plugin/src/main/kotlin/godot/gradle/tasks/PackageMainJar.kt
+++ kt/plugins/godot-gradle-plugin/src/main/kotlin/godot/gradle/tasks/PackageMainJar.kt
@@ -8,18 +8,21 @@ open class PackageMainJar: ShadowJar(), GodotJvmTask {
         group = "godot-kotlin-jvm"
         description = "Creates a fat jar containing game code and all dependencies of it"
 
-        archiveBaseName.set("main")
-        archiveVersion.set("")
-        archiveClassifier.set("")
+        val shadowJarTask = project.tasks.named("shadowJar", ShadowJar::class.java).get().apply {
+            archiveBaseName.set("main")
+            archiveVersion.set("")
+            archiveClassifier.set("")
 
-        dependencies {
-            it.exclude(it.dependency("org.jetbrains.kotlin:kotlin-stdlib.*"))
-            it.exclude(it.dependency("com.utopia-rise:godot-library:.*"))
+            dependencies {
+                it.exclude(it.dependency("org.jetbrains.kotlin:kotlin-stdlib.*"))
+                it.exclude(it.dependency("com.utopia-rise:godot-library:.*"))
+            }
         }
 
         dependsOn(
             TaskRegistry.CREATE_BUILD_LOCK.taskName,
             TaskRegistry.GENERATE_ENTRY_SERVICE_FILE.taskName
         )
+        finalizedBy(shadowJarTask)
     }
 }
