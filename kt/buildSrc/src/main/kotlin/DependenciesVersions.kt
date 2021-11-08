af074d99deefc304d3b35c702e7a1c7b4c1c7de1
Pierre-Thomas Meisels <meisels27@yahoo.fr>
2021-11-08 13:23:32 +0100

    Update mention to godot version in CI and jvm code

--- kt/buildSrc/src/main/kotlin/DependenciesVersions.kt
+++ kt/buildSrc/src/main/kotlin/DependenciesVersions.kt
@@ -1,5 +1,5 @@
 object DependenciesVersions {
-    const val godotVersion: String = "3.3.4"
+    const val godotVersion: String = "3.4.0"
     const val shadowJarPluginVersion: String = "6.1.0"
     const val kotlinPoetVersion: String = "1.8.0"
     const val kspVersion: String = "1.5.21-1.0.0-beta05"
