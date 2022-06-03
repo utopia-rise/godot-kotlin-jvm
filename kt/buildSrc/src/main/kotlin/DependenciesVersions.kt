642b4f28687ea345a2b99d4157ef836b778eba3a
Pierre-Thomas Meisels <meisels27@yahoo.fr>
2022-06-03 17:51:21 +0200

    Security: Update shadow plugin dependency to 7.1.2, in order to address [CVE-2021-45105](https://github.com/advisories/GHSA-p6xc-xr62-6r2g) and [CVE-2021-44832](https://github.com/advisories/GHSA-8489-44mv-ggj8)

--- kt/buildSrc/src/main/kotlin/DependenciesVersions.kt
+++ kt/buildSrc/src/main/kotlin/DependenciesVersions.kt
@@ -1,6 +1,6 @@
 object DependenciesVersions {
     const val godotVersion: String = "3.4.4"
-    const val shadowJarPluginVersion: String = "6.1.0"
+    const val shadowJarPluginVersion: String = "7.1.2"
     const val kotlinPoetVersion: String = "1.8.0"
     const val kspVersion: String = "1.6.0-1.0.1"
     const val supportedKotlinVersion: String = "1.6.0"
