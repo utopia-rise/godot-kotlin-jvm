362e4bc599cb1eb6539120bd070941237f10ef06
Cedric Hippmann <cedric.hippmann@hotmail.com>
2022-08-11 10:47:11 +0200

    Remove old unneeded additional source set

--- kt/plugins/godot-intellij-plugin/src/IJ183/kotlin/godot/intellij/plugin/extension/moduleExt.kt
+++
@@ -1,6 +0,0 @@
-package godot.intellij.plugin.extension
-
-import com.intellij.openapi.module.Module
-import org.jetbrains.kotlin.idea.framework.isGradleModule
-
-fun Module.isGradleModule(): Boolean = this.isGradleModule()
