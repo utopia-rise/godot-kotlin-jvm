utopia-rise/godot-kotlin-jvm

commit 5e2fdc377da7834215bbc8597f843b3b3856c7a9

Feature/update to kotlin 1.6.21 and add deployment IJ213 intellij plugin (#324)
* chore: update godot kotlin module version to 0.3.5

* enh: Add deployment of intellij plugin for 2021.3 version

* feat: Update module to kotlin 1.6.21

kt/entry-generation/godot-kotlin-symbol-processor/src/main/kotlin/godot/annotation/processor/compiler/KtCallExpressionExtractor.kt
@@ -16,7 +16,7 @@ import org.jetbrains.kotlin.psi.psiUtil.containingClassOrObject
 import org.jetbrains.kotlin.psi.psiUtil.referenceExpression
 import org.jetbrains.kotlin.resolve.BindingContext
 import org.jetbrains.kotlin.resolve.bindingContextUtil.getReferenceTargets
-import org.jetbrains.kotlin.resolve.calls.callUtil.getType
+import org.jetbrains.kotlin.resolve.calls.util.getType
 import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameSafe
 import org.jetbrains.kotlin.serialization.deserialization.descriptors.DeserializedClassConstructorDescriptor
 import org.jetbrains.kotlin.serialization.deserialization.descriptors.DeserializedSimpleFunctionDescriptor

kt/plugins/godot-intellij-plugin/build.gradle.kts
@@ -19,38 +19,29 @@ plugins {
 //sdk version: https://github.com/JetBrains/intellij-community/tags
 //kotlin plugin version: https://plugins.jetbrains.com/plugin/6954-kotlin/versions
 val buildMatrix: Map<String, BuildConfig> = mapOf(
-    "IJ203" to BuildConfig(
-        sdk = "203.8084.24",
-        prefix = "IJ2020.3",
-        extraSource = "IJ183",
-        version = VersionRange("203.1", "203.*"),
-        ideVersionsForVerifierTask = listOf("2020.1.4", "2020.2.3", "2020.3"),
-        deps = listOf("java", "org.jetbrains.kotlin:203-1.5.21-release-316-IJ7717.8", "gradle")
-    ),
     "IJ211" to BuildConfig(
-        sdk = "211.7628.21",
+        sdk = "211.7442.40",
         prefix = "IJ2021.1",
         extraSource = "IJ183",
-        version = VersionRange("211.1", "211.*"),
+        version = VersionRange("211.2", "211.*"),
         ideVersionsForVerifierTask = listOf("2021.1.1", "2021.1.2", "2021.1.3"),
-        deps = listOf("java", "org.jetbrains.kotlin:211-1.5.21-release-317-IJ7442.40", "gradle")
+        deps = listOf("java", "org.jetbrains.kotlin:211-1.6.21-release-334-IJ7442.40", "gradle")
     ),
     "IJ212" to BuildConfig(
-        sdk = "212.5080.55",
+        sdk = "212.5457.46",
         prefix = "IJ2021.2",
         extraSource = "IJ183",
-        version = VersionRange("212.1", "212.*"),
+        version = VersionRange("212.3", "212.*"),
         ideVersionsForVerifierTask = listOf("2021.2.1"),
-        deps = listOf("java", "org.jetbrains.kotlin:212-1.5.30-release-409-IJ4638.7", "gradle")
+        deps = listOf("java", "org.jetbrains.kotlin:212-1.6.21-release-334-IJ5457.46", "gradle")
     ),
     "IJ213" to BuildConfig(
-        sdk = "213.5744.223",
+        sdk = "213.6777.52",
         prefix = "IJ2021.3",
         extraSource = "IJ213",
-        version = VersionRange("212.1", "213.*"),
+        version = VersionRange("213.2", "213.*"),
         ideVersionsForVerifierTask = listOf("2021.3"),
-        //TODO: replace kotlin plugin version with release channel once released by Jetbrains
-        deps = listOf("java", "org.jetbrains.kotlin:213-1.5.10-release-IJ5333@Ideadev", "gradle")
+        deps = listOf("java", "org.jetbrains.kotlin:213-1.6.21-release-334-IJ6777.52", "gradle")
     )
 )
 

kt/plugins/godot-intellij-plugin/src/main/kotlin/godot/intellij/plugin/annotator/property/KtExpressionConstantChecker.kt
@@ -17,7 +17,7 @@ import org.jetbrains.kotlin.psi.KtOperationReferenceExpression
 import org.jetbrains.kotlin.psi.KtStringTemplateExpression
 import org.jetbrains.kotlin.psi.psiUtil.referenceExpression
 import org.jetbrains.kotlin.resolve.bindingContextUtil.getReferenceTargets
-import org.jetbrains.kotlin.resolve.calls.callUtil.getType
+import org.jetbrains.kotlin.resolve.calls.util.getType
 import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameSafe
 import org.jetbrains.kotlin.resolve.descriptorUtil.isCompanionObject
 import org.jetbrains.kotlin.resolve.lazy.BodyResolveMode

harness/tests/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     // no need to apply kotlin jvm plugin. Our plugin already applies the correct version for you
-//    kotlin("jvm") version "1.6.0"
+//    kotlin("jvm") version "1.6.21"
     id("com.utopia-rise.godot-kotlin-jvm")
 }
 

kt/api-generator/build.gradle.kts
@@ -24,7 +24,7 @@ gradlePlugin {
 }
 
 dependencies {
-    implementation(kotlin("gradle-plugin", version = "1.6.0"))
+    implementation(kotlin("gradle-plugin", version = "1.6.21"))
     implementation("com.squareup:kotlinpoet:1.8.0")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.11.0")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.10.1")

kt/buildSrc/build.gradle.kts
@@ -25,5 +25,5 @@ gradlePlugin {
 }
 
 dependencies {
-    implementation(kotlin("gradle-plugin", version = "1.6.0"))
+    implementation(kotlin("gradle-plugin", version = "1.6.21"))
 }

.github/workflows/check-pr-intellij-plugin.yaml
@@ -8,7 +8,7 @@ jobs:
   build-intellij-plugin:
     strategy:
       matrix:
-        ij_sdk: [IJ203, IJ211, IJ212]
+        ij_sdk: [ IJ211, IJ212, IJ213 ]
     runs-on: ubuntu-latest
     steps:
       - name: Clone Godot JVM module.

.github/workflows/deploy-intellij-plugin.yaml
@@ -10,7 +10,7 @@ jobs:
   deploy_godot_intellij_plugin:
     strategy:
       matrix:
-        ij_sdk: [ IJ203, IJ211, IJ212 ]
+        ij_sdk: [ IJ211, IJ212, IJ213 ]
     runs-on: ubuntu-latest
     steps:
       - name: Clone Godot JVM module.

commit 9e7e1d76a2e2e06aca4f1b14a167fe792baaf7cd

Fix compatibility issues for old plugin version which broke with commit 5e2fdc377da7834215bbc8597f843b3b3856c7a9

kt/entry-generation/godot-kotlin-symbol-processor/src/main/kotlin/godot/annotation/processor/compiler/KtCallExpressionExtractor.kt
@@ -14,7 +14,7 @@ import org.jetbrains.kotlin.psi.psiUtil.containingClassOrObject
 import org.jetbrains.kotlin.psi.psiUtil.referenceExpression
 import org.jetbrains.kotlin.resolve.BindingContext
 import org.jetbrains.kotlin.resolve.bindingContextUtil.getReferenceTargets
-import org.jetbrains.kotlin.resolve.calls.util.getType
+import godot.intellij.plugin.extension.getType
 import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameSafe
 
 object KtCallExpressionExtractor {

kt/plugins/godot-intellij-plugin/build.gradle.kts
@@ -29,7 +29,7 @@ val buildMatrix: Map<String, BuildConfig> = mapOf(
     "IJ221" to BuildConfig(
         sdk = "221.5591.52",
         prefix = "IJ2022.1",
-        extraSource = "IJ213", // hasn't changed. Thus no need to update
+        extraSource = "IJ221",
         version = VersionRange("221.3", "999.*"),
         ideVersionsForVerifierTask = listOf("2022.1"),
         deps = listOf("java", "org.jetbrains.kotlin:221-1.7.10-release-333-IJ5591.52", "gradle")
@@ -37,7 +37,7 @@ val buildMatrix: Map<String, BuildConfig> = mapOf(
     "IJ222" to BuildConfig(
         sdk = "222.3345.118",
         prefix = "IJ2022.2",
-        extraSource = "IJ213", // hasn't changed. Thus no need to update
+        extraSource = "IJ221", // hasn't changed. Thus no need to update
         version = VersionRange("222.2", "999.*"),
         ideVersionsForVerifierTask = listOf("2022.2"),
         deps = listOf("java", "org.jetbrains.kotlin", "gradle") // kotlin plugin version no longer needed as it's now bundled with the IDE

kt/plugins/godot-intellij-plugin/src/IJ213/kotlin/godot/intellij/plugin/extension/ktExpressionExt.kt
@@ -1,0 +1,10 @@
+package godot.intellij.plugin.extension
+
+import org.jetbrains.kotlin.psi.KtExpression
+import org.jetbrains.kotlin.resolve.BindingContext
+import org.jetbrains.kotlin.resolve.calls.callUtil.getType
+import org.jetbrains.kotlin.types.KotlinType
+import org.jetbrains.kotlin.utils.IDEAPluginsCompatibilityAPI
+
+@OptIn(IDEAPluginsCompatibilityAPI::class) // needed as the kotlin version used together with this plugin version does not yet include the replacement for this method. False positive
+fun KtExpression.getType(context: BindingContext): KotlinType? = this.getType(context)

kt/plugins/godot-intellij-plugin/src/IJ213/kotlin/godot/intellij/plugin/extension/psiElementExt.kt
@@ -1,0 +1,8 @@
+package godot.intellij.plugin.extension
+
+import com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.idea.refactoring.fqName.getKotlinFqName
+import org.jetbrains.kotlin.name.FqName
+
+@Suppress("DEPRECATION") // new variant does not exist yet in this sdk version. False positive
+fun PsiElement.getKotlinFqName(): FqName? = this.getKotlinFqName()

kt/plugins/godot-intellij-plugin/src/IJ221/kotlin/godot/intellij/plugin/extension/ktExpressionExt.kt
@@ -1,0 +1,8 @@
+package godot.intellij.plugin.extension
+
+import org.jetbrains.kotlin.psi.KtExpression
+import org.jetbrains.kotlin.resolve.BindingContext
+import org.jetbrains.kotlin.resolve.calls.util.getType
+import org.jetbrains.kotlin.types.KotlinType
+
+fun KtExpression.getType(context: BindingContext): KotlinType? = this.getType(context)

kt/plugins/godot-intellij-plugin/src/IJ221/kotlin/godot/intellij/plugin/extension/moduleExt.kt
@@ -1,0 +1,6 @@
+package godot.intellij.plugin.extension
+
+import com.intellij.openapi.module.Module
+import org.jetbrains.kotlin.idea.configuration.isGradleModule
+
+fun Module.isGradleModule(): Boolean = this.isGradleModule()

kt/plugins/godot-intellij-plugin/src/IJ221/kotlin/godot/intellij/plugin/extension/psiElementExt.kt
@@ -1,0 +1,7 @@
+package godot.intellij.plugin.extension
+
+import com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.idea.base.utils.fqname.getKotlinFqName
+import org.jetbrains.kotlin.name.FqName
+
+fun PsiElement.getKotlinFqName(): FqName? = this.getKotlinFqName()

kt/plugins/godot-intellij-plugin/src/main/kotlin/godot/intellij/plugin/annotator/property/KtExpressionConstantChecker.kt
@@ -1,5 +1,6 @@
 package godot.intellij.plugin.annotator.property
 
+import godot.intellij.plugin.extension.getType
 import org.jetbrains.kotlin.backend.common.serialization.findPackage
 import org.jetbrains.kotlin.builtins.KotlinBuiltIns
 import org.jetbrains.kotlin.descriptors.impl.ClassConstructorDescriptorImpl
@@ -17,7 +18,6 @@ import org.jetbrains.kotlin.psi.KtOperationReferenceExpression
 import org.jetbrains.kotlin.psi.KtStringTemplateExpression
 import org.jetbrains.kotlin.psi.psiUtil.referenceExpression
 import org.jetbrains.kotlin.resolve.bindingContextUtil.getReferenceTargets
-import org.jetbrains.kotlin.resolve.calls.util.getType
 import org.jetbrains.kotlin.resolve.descriptorUtil.fqNameSafe
 import org.jetbrains.kotlin.resolve.descriptorUtil.isCompanionObject
 import org.jetbrains.kotlin.resolve.lazy.BodyResolveMode
@@ -26,7 +26,7 @@ class KtExpressionConstantChecker {
     fun isConstantEnoughForRegistration(ktExpression: KtExpression): Boolean {
         return if (!ktExpression.isConstant()) {
             return when (ktExpression) {
-                is KtCallExpression -> checkKtCallExpresion(ktExpression)
+                is KtCallExpression -> checkKtCallExpression(ktExpression)
                 is KtDotQualifiedExpression -> checkKtDotQualifiedExpression(ktExpression)
                 is KtStringTemplateExpression -> !ktExpression.hasInterpolation()
                 is KtBinaryExpression -> checkKtBinaryExpression(ktExpression)
@@ -35,7 +35,7 @@ class KtExpressionConstantChecker {
         } else true
     }
 
-    private fun checkKtCallExpresion(ktExpression: KtCallExpression): Boolean {
+    private fun checkKtCallExpression(ktExpression: KtCallExpression): Boolean {
         val bindingContext = ktExpression.analyze(BodyResolveMode.FULL)
         return ktExpression
             .referenceExpression()

kt/plugins/godot-intellij-plugin/src/main/kotlin/godot/intellij/plugin/annotator/reference/RSetPropertyReferenceChecker.kt
@@ -3,9 +3,9 @@ package godot.intellij.plugin.annotator.reference
 import com.intellij.lang.annotation.AnnotationHolder
 import godot.intellij.plugin.GodotPluginBundle
 import godot.intellij.plugin.data.model.REGISTER_PROPERTY_ANNOTATION
+import godot.intellij.plugin.extension.getKotlinFqName
 import godot.intellij.plugin.extension.registerProblem
 import godot.intellij.plugin.quickfix.TargetPropertyNotRegisteredQuickFix
-import org.jetbrains.kotlin.idea.base.utils.fqname.getKotlinFqName
 import org.jetbrains.kotlin.idea.util.findAnnotation
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.nj2k.postProcessing.resolve

kt/plugins/godot-intellij-plugin/src/main/kotlin/godot/intellij/plugin/annotator/reference/RpcFunctionReferenceChecker.kt
@@ -3,9 +3,9 @@ package godot.intellij.plugin.annotator.reference
 import com.intellij.lang.annotation.AnnotationHolder
 import godot.intellij.plugin.GodotPluginBundle
 import godot.intellij.plugin.data.model.REGISTER_FUNCTION_ANNOTATION
+import godot.intellij.plugin.extension.getKotlinFqName
 import godot.intellij.plugin.extension.registerProblem
 import godot.intellij.plugin.quickfix.TargetFunctionNotRegisteredQuickFix
-import org.jetbrains.kotlin.idea.base.utils.fqname.getKotlinFqName
 import org.jetbrains.kotlin.idea.util.findAnnotation
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.nj2k.postProcessing.resolve

kt/plugins/godot-intellij-plugin/src/main/kotlin/godot/intellij/plugin/listener/KtPsiTreeListener.kt
@@ -12,12 +12,12 @@ import com.intellij.psi.search.GlobalSearchScope
 import godot.intellij.plugin.ProjectDisposable
 import godot.intellij.plugin.data.cache.classname.RegisteredClassNameCacheProvider
 import godot.intellij.plugin.extension.getGodotRoot
+import godot.intellij.plugin.extension.getKotlinFqName
 import godot.intellij.plugin.extension.isInGodotRoot
 import godot.intellij.plugin.refactor.SceneAction
 import godot.intellij.plugin.wrapper.PsiTreeChangeListenerKt
 import org.jetbrains.kotlin.idea.KotlinFileType
 import org.jetbrains.kotlin.idea.KotlinLanguage
-import org.jetbrains.kotlin.idea.base.utils.fqname.getKotlinFqName
 import org.jetbrains.kotlin.psi.KtClass
 import org.jetbrains.kotlin.psi.KtFile
 