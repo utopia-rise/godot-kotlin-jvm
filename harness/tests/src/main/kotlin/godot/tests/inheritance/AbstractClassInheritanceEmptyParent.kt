05d7b20d11a98ad9b4ae6f55a2bd8266bfac3c62
Cedric Hippmann <cedric.hippmann@hotmail.com>
2022-10-30 13:53:09 +0100

    Fix registration of empty abstract classes extending godot classes

---
+++ harness/tests/src/main/kotlin/godot/tests/inheritance/AbstractClassInheritanceEmptyParent.kt
@@ -0,0 +1,6 @@
+package godot.tests.inheritance
+
+import godot.KinematicBody
+
+// issue: https://github.com/utopia-rise/godot-kotlin-jvm/issues/365
+abstract class AbstractClassInheritanceEmptyParent: KinematicBody()
