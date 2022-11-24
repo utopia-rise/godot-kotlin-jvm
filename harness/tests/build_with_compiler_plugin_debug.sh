858e792f66b1ec2c6465c7da8fd3d44de342eb8b
Cedric Hippmann <cedric.hippmann@hotmail.com>
2022-11-24 10:23:39 +0100

    Add script with gradle command for debugging the entry gen

---
+++ harness/tests/build_with_compiler_plugin_debug.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+./gradlew build --no-daemon -Dorg.gradle.debug=true -Dkotlin.compiler.execution.strategy="in-process" -Dkotlin.daemon.jvm.options="-Xdebug,-Xrunjdwp:transport=dt_socket,address=5005,server=y,suspend=n"
