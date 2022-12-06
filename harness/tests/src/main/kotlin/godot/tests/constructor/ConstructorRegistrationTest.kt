6384dc4f2c76d1debe3d5216de422825da4c61ea
Cedric Hippmann <cedric.hippmann@hotmail.com>
2022-12-06 07:23:51 +0100

    Fix constructor with generic args registration (#396)
    * Fix registration of constructors with multiple args
    
    * Add test class

---
+++ harness/tests/src/main/kotlin/godot/tests/constructor/ConstructorRegistrationTest.kt
@@ -0,0 +1,14 @@
+package godot.tests.constructor
+
+import godot.Node
+import godot.annotation.RegisterClass
+import godot.annotation.RegisterConstructor
+import godot.core.Dictionary
+import godot.core.VariantArray
+
+@RegisterClass
+class ConstructorRegistrationTest(): Node() {
+
+    @RegisterConstructor
+    constructor(param1: Dictionary<Any, Any?>, param2: VariantArray<Long>): this()
+}
