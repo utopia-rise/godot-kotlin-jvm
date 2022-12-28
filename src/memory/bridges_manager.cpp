4c4c4709ce2eca0373b3dd1133b40057581bad95
Pierre-Thomas Meisels <meisels27@yahoo.fr>
2022-12-28 13:27:39 +0100

    fix: initialize callable bridge in BridgeManager

--- src/memory/bridges_manager.cpp
+++ src/memory/bridges_manager.cpp
@@ -12,6 +12,7 @@ void BridgesManager::initialize_bridges(jni::Env& env, jni::JObject class_loader
 
     initialize_bridge(env, class_loader, bridges::GD_PRINT_BRIDGE_CLASS_NAME, gd_print_bridge);
 
+    initialize_bridge(env, class_loader, bridges::CALLABLE_BRIDGE_CLASS_NAME, callable_bridge);
     initialize_bridge(env, class_loader, bridges::VARIANT_ARRAY_BRIDGE_CLASS_NAME, variant_array_bridge);
     initialize_bridge(env, class_loader, bridges::DICTIONARY_BRIDGE_CLASS_NAME, dictionary_bridge);
     initialize_bridge(env, class_loader, bridges::RID_BRIDGE_CLASS_NAME, rid_bridge);
@@ -32,6 +33,7 @@ void BridgesManager::initialize_bridges(jni::Env& env, jni::JObject class_loader
 BridgesManager::BridgesManager() :
   memory_bridge(nullptr),
   gd_print_bridge(nullptr),
+  callable_bridge(nullptr),
   variant_array_bridge(nullptr),
   dictionary_bridge(nullptr),
   rid_bridge(nullptr),
@@ -53,6 +55,9 @@ void BridgesManager::delete_bridges() {
 
     delete gd_print_bridge;
     gd_print_bridge = nullptr;
+
+    delete callable_bridge;
+    callable_bridge = nullptr;
 
     delete variant_array_bridge;
     variant_array_bridge = nullptr;
