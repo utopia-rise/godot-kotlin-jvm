775779e04567d172f938b716b928783628a319e3
chippmann <cedric.hippmann@hotmail.com>
2023-03-01 09:12:18 +0100

    Remove psi based default value extraction

--- kt/entry-generation/godot-kotlin-symbol-processor/src/main/kotlin/godot/annotation/processor/compiler/KtOperationReferenceExpressionExtractor.kt
+++
@@ -1,8 +0,0 @@
-package godot.annotation.processor.compiler
-
-import org.jetbrains.kotlin.psi.KtOperationReferenceExpression
-
-object KtOperationReferenceExpressionExtractor {
-
-    fun extract(expression: KtOperationReferenceExpression) = "%L" to arrayOf(expression.text)
-}
