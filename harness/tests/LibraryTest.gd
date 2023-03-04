5a8c916d7fcc3b4c2d9481661993242ced971de9
chippmann <cedric.hippmann@hotmail.com>
2023-03-04 15:43:51 +0100

    Add library test

--- harness/tests/LibraryTest.gd
+++ harness/tests/LibraryTest.gd
@@ -1,9 +1,12 @@
 extends Node3D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	print("MESSAGES:")
-	print($HierarchicalLibrary.provide_greeting())
-	print($FlattenedLibrary.provide_greeting())
-	print($FqNameLibrary.provide_greeting())
+func provide_greeting_for_hierarchical_library() -> String:
+	return $HierarchicalLibrary.provide_greeting()
+
+
+func provide_greeting_for_flattened_library() -> String:
+	return $FlattenedLibrary.provide_greeting()
+
+func provide_greeting_for_fqname_library() -> String:
+	return $FqNameLibrary.provide_greeting()
---
+++ harness/tests/test/unit/test_godot_kotlin_jvm_dependencies.gd
@@ -0,0 +1,9 @@
+extends "res://addons/gut/test.gd"
+
+
+func test_call_without_param():
+	var library_test_scene = load("res://library_test.tscn").instantiate()
+	assert_eq(library_test_scene.provide_greeting_for_hierarchical_library(), "Hello from hierarchical-library-test!", "Greeting from hierarchical library does not match")
+	assert_eq(library_test_scene.provide_greeting_for_flattened_library(), "Hello from flattened-library-test!", "Greeting from flattened library does not match")
+	assert_eq(library_test_scene.provide_greeting_for_fqname_library(), "Hello from fqname-library-test!", "Greeting from fqname library does not match")
+	library_test_scene.free()
