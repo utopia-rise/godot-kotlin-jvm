c56a60ba6acf14e9ccea76e6ccf84fe0553daa49
chippmann <cedric.hippmann@hotmail.com>
2023-03-04 15:48:06 +0100

    Enter properties with snake case in dummy files

--- kt/entry-generation/godot-entry-generator/src/main/kotlin/godot/entrygenerator/filebuilder/DummyFileGenerator.kt
+++ kt/entry-generation/godot-entry-generator/src/main/kotlin/godot/entrygenerator/filebuilder/DummyFileGenerator.kt
@@ -1,6 +1,7 @@
 package godot.entrygenerator.filebuilder
 
 import godot.entrygenerator.model.RegisteredClassMetadataContainer
+import godot.util.camelToSnakeCase
 import java.io.BufferedWriter
 
 class DummyFileGenerator(
@@ -19,13 +20,13 @@ class DummyFileGenerator(
                     |    ${metadata.superTypes.split(",").joinToString(",\n\t") { it.trim() }}
                     |]
                     |signals = [
-                    |    ${metadata.signals.split(",").joinToString(",\n\t") { it.substringAfterLast(".").trim() }}
+                    |    ${metadata.signals.split(",").joinToString(",\n\t") { it.substringAfterLast(".").trim().camelToSnakeCase() }}
                     |]
                     |properties = [
-                    |    ${metadata.properties.split(",").joinToString(",\n\t") { it.substringAfterLast(".").trim() }}
+                    |    ${metadata.properties.split(",").joinToString(",\n\t") { it.substringAfterLast(".").trim().camelToSnakeCase() }}
                     |]
                     |functions = [
-                    |    ${metadata.functions.split(",").joinToString(",\n\t") { it.substringAfterLast(".").trim() }}
+                    |    ${metadata.functions.split(",").joinToString(",\n\t") { it.substringAfterLast(".").trim().camelToSnakeCase() }}
                     |]
                 """.trimMargin()
             )
