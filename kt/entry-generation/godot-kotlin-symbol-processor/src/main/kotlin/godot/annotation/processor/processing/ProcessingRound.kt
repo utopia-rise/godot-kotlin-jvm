698d8fc9d0f9e432ca6cf725ae42a69b866198cf
Cedric Hippmann <cedric.hippmann@hotmail.com>
2023-03-06 16:52:18 +0100

    Make purpose of processing round clearer

--- kt/entry-generation/godot-kotlin-symbol-processor/src/main/kotlin/godot/annotation/processor/processing/ProcessingRound.kt
+++ kt/entry-generation/godot-kotlin-symbol-processor/src/main/kotlin/godot/annotation/processor/processing/ProcessingRound.kt
@@ -4,7 +4,7 @@ package godot.annotation.processor.processing
  * Current processing round indicator. Mainly used for human-readable form of process round count
  */
 internal enum class ProcessingRound {
-    GENERATE_REGISTRARS_AND_REGISTRATION_FILES_FOR_DEPENDENCIES,
+    GENERATE_REGISTRARS_FOR_THIS_PROJECT_AND_REGISTRATION_FILES_FOR_DEPENDENCIES,
     GENERATE_REGISTRATION_FILES_FOR_REGISTRARS,
     UPDATE_REGISTRATION_FILES
 }
