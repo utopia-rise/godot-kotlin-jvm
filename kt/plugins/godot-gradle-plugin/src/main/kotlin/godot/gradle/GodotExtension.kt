9e1109ed8304523ffb276810f6a8540c48640fb3
Cedric Hippmann <cedric.hippmann@hotmail.com>
2023-03-06 13:39:49 +0100

    Add kdoc and specify types explicitly

--- kt/plugins/godot-gradle-plugin/src/main/kotlin/godot/gradle/GodotExtension.kt
+++ kt/plugins/godot-gradle-plugin/src/main/kotlin/godot/gradle/GodotExtension.kt
@@ -6,11 +6,70 @@ import org.gradle.api.provider.Property
 import java.io.File
 
 open class GodotExtension(objects: ObjectFactory) {
+    /**
+     * Registers every class with this prefix. Especially useful for library authors
+     */
     val classPrefix: Property<String> = objects.property(String::class.java)
+
+    /**
+     * Base directory where registration files ('.gdj' files) are generated to. Defaults to `<projectDir>/gdj`
+     */
     val registrationFileBaseDir: RegularFileProperty = objects.fileProperty()
+
+    /**
+     * Defines whether the registration files follow the package hierarchy or if they are all generated to the [registrationFileBaseDir]. Defaults to `true`
+     *
+     * Examples:
+     *
+     * **true**:
+     * ```
+     *  -[registrationFileBaseDir]
+     *      | packagePathOne
+     *          | ClassOne.gdj
+     *          | ClassTwo.gdj
+     *      | packagePathTwo
+     *          | ClassThree.gdj
+     *          | ClassFour.gdj
+     * ```
+     * **false**:
+     * ```
+     *  -[registrationFileBaseDir]
+     *      | ClassOne.gdj
+     *      | ClassTwo.gdj
+     *      | ClassThree.gdj
+     *      | ClassFour.gdj
+     * ```
+     */
     val isRegistrationFileHierarchyEnabled: Property<Boolean> = objects.property(Boolean::class.java)
+
+    /**
+     * Defines whether classes should be registered with the full fqName or just with their simple name. Defaults to false
+     *
+     * **Note:** the custom class name in the `@RegisterClass` annotation takes precedence over this property!
+     *
+     * Examples:
+     *
+     * **true**: `com.company.MyClass` -> `com_company_MyClass`
+     *
+     * **false**: `com.company.MyClass` -> `MyClass`
+     */
     val isFqNameRegistrationEnabled: Property<Boolean> = objects.property(Boolean::class.java)
+
+    /**
+     * Only has a visible effect when this project is used as a library. This project name defines to what directory the
+     * registration files are generated to.
+     *
+     * Example:
+     * ```
+     *  -[registrationFileBaseDir]
+     *      | dependencies
+     *          | <your_defined_project_name>
+     *              | ClassFromThisProject.gdj
+     *      | ClassFromConsumingProject.gdj
+     *  ```
+     */
     val projectName: Property<String> = objects.property(String::class.java)
+
     /**
      * enable android export
      *
@@ -23,35 +82,35 @@ open class GodotExtension(objects: ObjectFactory) {
      *
      * example: "${System.getenv("ANDROID_SDK_ROOT")}/build-tools/31.0.0/d8"
      */
-    var d8ToolPath = objects.fileProperty()
+    var d8ToolPath: RegularFileProperty = objects.fileProperty()
 
     /**
      * path to the sdk dir for your target sdk compilation dir
      *
      * example: "${System.getenv("ANDROID_SDK_ROOT")}/platforms/android-30"
      */
-    var androidCompileSdkDir = objects.fileProperty()
+    var androidCompileSdkDir: RegularFileProperty = objects.fileProperty()
 
     /**
      * Min android api version.
      *
      * example: 21
      */
-    var androidMinApi = objects.property(Int::class.java)
+    var androidMinApi: Property<Int> = objects.property(Int::class.java)
 
     /**
      * enable Graal Native Image Export
      *
      * if is set to true, native-image tool and graalvm home has to be resolvable
      */
-    val isGraalNativeImageExportEnabled = objects.property(Boolean::class.java)
+    val isGraalNativeImageExportEnabled: Property<Boolean> = objects.property(Boolean::class.java)
 
     /**
      * path to the native-image tool used to convert jar to native.
      *
      * example: "${System.getenv("GRAALVM_HOME")}/bin/native-image"
      */
-    val nativeImageToolPath = objects.fileProperty()
+    val nativeImageToolPath: RegularFileProperty = objects.fileProperty()
 
     /**
      * Windows specific.
@@ -59,21 +118,21 @@ open class GodotExtension(objects: ObjectFactory) {
      *
      * example: System.getenv("VC_VARS_PATH")
      */
-    val windowsDeveloperVCVarsPath = objects.property(String::class.java)
+    val windowsDeveloperVCVarsPath: Property<String> = objects.property(String::class.java)
 
     /**
      * Additional Graal JNI/reflection configurations.
      *
      * example: arrayOf("my-jni-configuration-file.json", "another-conf.json")
      */
-    val additionalGraalJniConfigurationFiles = objects.property(Array<String>::class.java)
+    val additionalGraalJniConfigurationFiles: Property<Array<String>> = objects.property(Array<String>::class.java)
 
     /**
      * enable verbose mode on native image generation.
      *
      * if set to true, native-image tool will be in verbose mode.
      */
-    val isGraalVmNativeImageGenerationVerbose = objects.property(Boolean::class.java)
+    val isGraalVmNativeImageGenerationVerbose: Property<Boolean> = objects.property(Boolean::class.java)
 
     internal fun configureExtensionDefaults() {
         val buildToolsDir = System.getenv("ANDROID_SDK_ROOT")?.let { androidSdkRoot ->
