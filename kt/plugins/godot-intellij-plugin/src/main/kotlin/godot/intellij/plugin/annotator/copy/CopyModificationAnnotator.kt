5be1030487f31d81d29d2cda6edc03c84da0a20c
chippmann <cedric.hippmann@hotmail.com>
2023-03-15 18:50:14 +0100

    Update imports

--- kt/plugins/godot-intellij-plugin/src/main/kotlin/godot/intellij/plugin/annotator/copy/CopyModificationAnnotator.kt
+++ kt/plugins/godot-intellij-plugin/src/main/kotlin/godot/intellij/plugin/annotator/copy/CopyModificationAnnotator.kt
@@ -11,9 +11,9 @@ import godot.intellij.plugin.extension.isInGodotRoot
 import godot.intellij.plugin.extension.registerProblem
 import godot.intellij.plugin.extension.resolve
 import org.jetbrains.kotlin.descriptors.impl.ClassConstructorDescriptorImpl
+import org.jetbrains.kotlin.idea.base.util.module
 import org.jetbrains.kotlin.idea.caches.resolve.analyze
 import org.jetbrains.kotlin.idea.util.findAnnotation
-import org.jetbrains.kotlin.idea.util.module
 import org.jetbrains.kotlin.js.descriptorUtils.getJetTypeFqName
 import org.jetbrains.kotlin.js.resolve.diagnostics.findPsi
 import org.jetbrains.kotlin.lexer.KtSingleValueToken
