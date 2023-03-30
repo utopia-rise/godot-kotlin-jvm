90447c2e741ced0a81cb510ffe9006707a440a60
Ced Naru <cednaru.dev@gmail.com>
2023-03-30 05:15:05 +0200

    Update instance test

--- harness/tests/test/unit/test_instance.gd
+++ harness/tests/test/unit/test_instance.gd
@@ -1,15 +1,15 @@
 extends "res://addons/gut/test.gd"
 
 func test_object_instance():
-	var obj: Object = ObjectInstance.new()
+	var obj: ObjectInstance = ObjectInstance.new()
 	assert_not_null(obj, "Object should not be null")
 	obj.free()
 
 func test_refcounted_instance():
-	var obj: Object = RefCountedInstance.new()
+	var obj: RefCountedInstance = RefCountedInstance.new()
 	assert_not_null(obj, "RefCounted should not be null")
 	
 func test_node_instance():
-	var obj: Object = Node.new()
+	var obj: NodeInstance = NodeInstance.new()
 	assert_not_null(obj, "Node should not be null")
 	obj.free()
