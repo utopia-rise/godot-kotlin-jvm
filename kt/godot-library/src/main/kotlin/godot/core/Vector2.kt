599dcfa106631d3d292bbb2aaf2201aeee3e26bf
Pierre-Thomas Meisels <meisels27@yahoo.fr>
2023-05-23 21:35:05 +0200

    feat(4.0): update Vector2 core type according to godot 4.0 api documentation

--- kt/godot-library/src/main/kotlin/godot/core/Vector2.kt
+++ kt/godot-library/src/main/kotlin/godot/core/Vector2.kt
@@ -94,6 +94,34 @@ class Vector2(
     }
 
     /**
+     * Returns the derivative at the given [t] on the Bézier curve defined by this vector and the given [control1],
+     * [control2], and [end] points.
+     */
+    fun bezierDerivative(
+        control1: Vector2,
+        control2: Vector2,
+        end: Vector2,
+        t: RealT
+    ) = Vector2(
+        bezierDerivative(this.x, control1.x, control2.x, end.x, t),
+        bezierDerivative(this.y, control1.y, control2.y, end.y, t)
+    )
+
+    /**
+     * Returns the point at the given [t] on the Bézier curve defined by this vector and the given [control1],
+     * [control2], and [end] points.
+     */
+    fun bezierInterpolate(
+        control1: Vector2,
+        control2: Vector2,
+        end: Vector2,
+        t: RealT
+    ) = Vector2(
+        bezierInterpolate(this.x, control1.x, control2.x, end.x, t),
+        bezierInterpolate(this.y, control1.y, control2.y, end.y, t)
+    )
+
+    /**
      * Returns the vector “bounced off” from a plane defined by the given normal.
      */
     fun bounce(n: Vector2): Vector2 {
@@ -229,11 +257,6 @@ class Vector2(
     }
 
     /**
-     * Returns true if this vector's values are approximately zero
-     */
-    fun isZeroApprox() = isEqualApprox(Vector2.ZERO)
-
-    /**
      * Returns true if this vector is finite, by calling @GlobalScope.is_finite on each component.
      */
     fun isFinite() = x.isFinite() && y.isFinite()
@@ -244,6 +267,11 @@ class Vector2(
     fun isNormalized(): Boolean {
         return isEqualApprox(this.length(), 1.0)
     }
+
+    /**
+     * Returns true if this vector's values are approximately zero
+     */
+    fun isZeroApprox() = isEqualApprox(Vector2.ZERO)
 
     /**
      * Returns the vector’s length.
