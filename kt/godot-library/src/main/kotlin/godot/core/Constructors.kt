bfbaa802ea1f0bd316dcd3ff9a5eb07f41ef37f1
chippmann <cedric.hippmann@hotmail.com>
2023-09-18 08:11:44 +0200

    Add new KtConstructor classes

--- kt/godot-library/src/main/kotlin/godot/core/Constructors.kt
+++ kt/godot-library/src/main/kotlin/godot/core/Constructors.kt
@@ -121,3 +121,72 @@ class KtConstructor5<T : KtObject, P0 : Any?, P1 : Any?, P2 : Any?, P3 : Any?, P
         )
     }
 }
+
+class KtConstructor6<T : KtObject, P0 : Any?, P1 : Any?, P2 : Any?, P3 : Any?, P4 : Any?, P5 : Any?>(
+    private val constructor: (P0, P1, P2, P3, P4, P5) -> T,
+    p0Type: Pair<VariantType, Boolean>,
+    p1Type: Pair<VariantType, Boolean>,
+    p2Type: Pair<VariantType, Boolean>,
+    p3Type: Pair<VariantType, Boolean>,
+    p4Type: Pair<VariantType, Boolean>,
+    p5Type: Pair<VariantType, Boolean>,
+) : KtConstructor<T>(p0Type, p1Type, p2Type, p3Type, p4Type, p5Type) {
+    override fun invoke(): T {
+        return constructor(
+            paramsArray[0] as P0,
+            paramsArray[1] as P1,
+            paramsArray[2] as P2,
+            paramsArray[3] as P3,
+            paramsArray[4] as P4,
+            paramsArray[5] as P5,
+        )
+    }
+}
+
+class KtConstructor7<T : KtObject, P0 : Any?, P1 : Any?, P2 : Any?, P3 : Any?, P4 : Any?, P5 : Any?, P6 : Any?>(
+    private val constructor: (P0, P1, P2, P3, P4, P5, P6) -> T,
+    p0Type: Pair<VariantType, Boolean>,
+    p1Type: Pair<VariantType, Boolean>,
+    p2Type: Pair<VariantType, Boolean>,
+    p3Type: Pair<VariantType, Boolean>,
+    p4Type: Pair<VariantType, Boolean>,
+    p5Type: Pair<VariantType, Boolean>,
+    p6Type: Pair<VariantType, Boolean>,
+) : KtConstructor<T>(p0Type, p1Type, p2Type, p3Type, p4Type, p5Type, p6Type) {
+    override fun invoke(): T {
+        return constructor(
+            paramsArray[0] as P0,
+            paramsArray[1] as P1,
+            paramsArray[2] as P2,
+            paramsArray[3] as P3,
+            paramsArray[4] as P4,
+            paramsArray[5] as P5,
+            paramsArray[6] as P6,
+        )
+    }
+}
+
+class KtConstructor8<T : KtObject, P0 : Any?, P1 : Any?, P2 : Any?, P3 : Any?, P4 : Any?, P5 : Any?, P6 : Any?, P7 : Any?>(
+    private val constructor: (P0, P1, P2, P3, P4, P5, P6, P7) -> T,
+    p0Type: Pair<VariantType, Boolean>,
+    p1Type: Pair<VariantType, Boolean>,
+    p2Type: Pair<VariantType, Boolean>,
+    p3Type: Pair<VariantType, Boolean>,
+    p4Type: Pair<VariantType, Boolean>,
+    p5Type: Pair<VariantType, Boolean>,
+    p6Type: Pair<VariantType, Boolean>,
+    p7Type: Pair<VariantType, Boolean>,
+) : KtConstructor<T>(p0Type, p1Type, p2Type, p3Type, p4Type, p5Type, p6Type, p7Type) {
+    override fun invoke(): T {
+        return constructor(
+            paramsArray[0] as P0,
+            paramsArray[1] as P1,
+            paramsArray[2] as P2,
+            paramsArray[3] as P3,
+            paramsArray[4] as P4,
+            paramsArray[5] as P5,
+            paramsArray[6] as P6,
+            paramsArray[7] as P7,
+        )
+    }
+}
