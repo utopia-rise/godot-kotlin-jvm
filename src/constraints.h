0872ede0f7de5bef221fbb978c3160ecc12d431a
chippmann <cedric.hippmann@hotmail.com>
2023-09-19 16:37:59 +0200

    Make constraints `static constexpr` to be sure they are inlined by the compiler

--- src/constraints.h
+++ src/constraints.h
@@ -6,9 +6,9 @@
 #define GODOT_JVM_CONSTRAINTS_H
 
 // when changed, also update godot.tools.common.constants.Constraints.MAX_CONSTRUCTOR_ARG_COUNT!
-const int MAX_CONSTRUCTOR_ARG_COUNT {8};
+static constexpr const int MAX_CONSTRUCTOR_ARG_COUNT {8};
 
 // when changed, also update godot.tools.common.constants.Constraints.MAX_FUNCTION_ARG_COUNT!
-const int MAX_FUNCTION_ARG_COUNT {8};
+static constexpr const int MAX_FUNCTION_ARG_COUNT {8};
 
 #endif //GODOT_JVM_CONSTRAINTS_H
