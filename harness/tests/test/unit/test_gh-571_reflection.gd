82f62d775007272b14bd8ce888bb72fd3b8f54f0
chippmann <cedric.hippmann@hotmail.com>
2024-02-07 06:58:15 +0100

    Fix reflection usage in exported projects

---
+++ harness/tests/test/unit/test_gh-571_reflection.gd
@@ -0,0 +1,7 @@
+extends "res://addons/gut/test.gd"
+
+
+func test_regression_test_gh-571() -> void:
+	var reflection_test_script = load("res://scripts/godot/tests/reflection/GH571_ReflectionTest.gdj").new()
+	assert_equals(reflection_test_script.deserialize_json(), "data_deserialized_through_reflection", "Deserialized data did not match expected data")
+	reflection_test_script.free()
