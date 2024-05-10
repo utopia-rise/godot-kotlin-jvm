c911c2af9b02b313e14c0528b09c688311e0c645
chippmann <cedric.hippmann@hotmail.com>
2024-05-10 12:39:02 +0200

    Fix extension method name

--- kt/godot-library/src/main/kotlin/godot/extensions/PackedSceneExt.kt
+++ kt/godot-library/src/main/kotlin/godot/extensions/PackedSceneExt.kt
@@ -5,7 +5,7 @@ import godot.Node
 import godot.PackedScene
 
 @Suppress("NOTHING_TO_INLINE", "UNCHECKED_CAST")
-inline fun <T : Node> PackedScene.instanceAs(
+inline fun <T : Node> PackedScene.instantiateAs(
     editState: PackedScene.GenEditState = PackedScene.GenEditState.GEN_EDIT_STATE_DISABLED
 ): T? {
     return instantiate(editState) as T?
