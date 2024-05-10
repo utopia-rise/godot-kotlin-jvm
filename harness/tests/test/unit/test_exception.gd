66bfaf517a2755ba52eb3cd6f7b326edfb36f5e5
chippmann <cedric.hippmann@hotmail.com>
2024-05-10 12:10:17 +0200

    Fix test function name

--- harness/tests/test/unit/test_exception.gd
+++ harness/tests/test/unit/test_exception.gd
@@ -1,7 +1,7 @@
 extends "res://addons/gut/test.gd"
 
 
-func test_reflection_working() -> void:
+func test_exception_printing_in_editor() -> void:
 	var exception_test_script = load("res://scripts/godot/tests/exception/ExceptionTest.gdj").new()
 	exception_test_script.throw_exception()
 	assert_true(true) # fake assert; exception printed to console and godot editor
