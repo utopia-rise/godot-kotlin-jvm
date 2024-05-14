b5cbe150fb703cfcd0248cf35003c1dac3b3e1f4
chippmann <cedric.hippmann@hotmail.com>
2024-05-14 12:47:53 +0200

    Implement automated execution and upload of benchmark data

--- harness/benchmarks/build.gradle.kts
+++ harness/benchmarks/build.gradle.kts
@@ -1,7 +1,107 @@
+import org.jetbrains.kotlin.konan.target.HostManager
+import java.net.HttpURLConnection
+import java.net.URL
+
 plugins {
     id("com.utopia-rise.godot-kotlin-jvm")
 }
 
 repositories {
     mavenCentral()
+}
+
+tasks {
+    val importResources = register<Exec>("importResources") {
+        dependsOn(compileKotlin)
+
+        isIgnoreExitValue = true
+
+        val editorExecutable: String = projectDir
+            .resolve("../../../../bin")
+            .listFiles()
+            ?.also {
+                println("[${it.joinToString()}]")
+            }
+            ?.firstOrNull { it.name.startsWith("godot.") && it.name.contains("editor") }
+            ?.absolutePath
+            ?: throw Exception("Could not find editor executable")
+
+        if (HostManager.hostIsMingw) {
+            commandLine(
+                "cmd",
+                "/c",
+                "$editorExecutable --headless --import",
+            )
+        } else {
+            commandLine(
+                "bash",
+                "-c",
+                "$editorExecutable --headless --import",
+            )
+        }
+    }
+    val runBenchmarks = register<Exec>("runBenchmarks") {
+        group = "validation"
+        dependsOn(importResources, compileKotlin)
+
+        isIgnoreExitValue = true
+
+        val editorExecutable: String = projectDir
+            .resolve("../../../../bin")
+            .listFiles()
+            ?.also {
+                println("[${it.joinToString()}]")
+            }
+            ?.firstOrNull { it.name.startsWith("godot.") && it.name.contains("editor") }
+            ?.absolutePath
+            ?: throw Exception("Could not find editor executable")
+
+        if (HostManager.hostIsMingw) {
+            commandLine(
+                "cmd",
+                "/c",
+                "$editorExecutable --headless -s scripts/Main.gd",
+            )
+        } else {
+            commandLine(
+                "bash",
+                "-c",
+                "$editorExecutable --headless -s scripts/Main.gd",
+            )
+        }
+    }
+    register("executeAndUploadBenchmarkResults") {
+        dependsOn(runBenchmarks)
+
+        doLast {
+            val benchmarkResultsFile = project.file("benchmark-results.json")
+
+            if (!benchmarkResultsFile.exists()) {
+                throw IllegalStateException("Cannot upload $benchmarkResultsFile as it does not exist. Make sure you ran ${runBenchmarks.name} first")
+            }
+
+            val uploadUrl = System.getenv("BENCHMARK_UPLOAD_URL")
+
+            val connection = URL(uploadUrl).openConnection() as HttpURLConnection
+            connection.requestMethod = "POST"
+            connection.doOutput = true
+            connection.setRequestProperty("Content-Type", "application/json")
+            connection.setRequestProperty("charset", "utf-8")
+            connection.setRequestProperty("Content-Length", benchmarkResultsFile.length().toString())
+            connection.useCaches = false
+
+            connection.outputStream.use { outputStream ->
+                benchmarkResultsFile.inputStream().use { inputStream ->
+                    inputStream.copyTo(outputStream)
+                }
+            }
+
+            val responseCode = connection.responseCode
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                println("POST was successful.")
+            } else {
+                println("POST not successful: $responseCode")
+            }
+        }
+    }
 }
--- harness/benchmarks/settings.gradle.kts
+++ harness/benchmarks/settings.gradle.kts
@@ -8,7 +8,8 @@ includeBuild("../../kt/api-generator") {
 includeBuild("../../kt") {
     dependencySubstitution {
         substitute(module("com.utopia-rise:godot-gradle-plugin")).using(project(":godot-gradle-plugin"))
-        substitute(module("com.utopia-rise:godot-library")).using(project(":godot-library"))
+        substitute(module("com.utopia-rise:godot-library-debug")).using(project(":godot-library"))
+        substitute(module("com.utopia-rise:godot-library-release")).using(project(":godot-library"))
         substitute(module("com.utopia-rise:godot-kotlin-symbol-processor")).using(project(":godot-kotlin-symbol-processor"))
         substitute(module("com.utopia-rise:godot-entry-generator")).using(project(":godot-entry-generator"))
     }
