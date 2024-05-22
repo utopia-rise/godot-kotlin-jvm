ccef2c5ce56a333bf99277cbdf1de2716150c79c
Cedric Hippmann <cedric.hippmann@hotmail.com>
2024-05-22 22:22:41 +0200

    Add better jni error reporting inside the godot editor (#627)
    * Add better jni error reporting
    
    * Describe exception in the case of a jni error as well.
    
    * Fix test function name
    
    * Remove unnecessary comments
    
    * Change init
    
    * Fix casing
    
    * Remove exception handling
    
    * Change tools_enabled to debug_enabled
    
    * Move variable declaration into if

--- src/jni/env.cpp
+++ src/jni/env.cpp
@@ -38,8 +38,34 @@ namespace jni {
         return env->ExceptionCheck();
     }
 
-    void Env::exception_describe() {
+    String Env::exception_describe() {
+#ifdef DEBUG_ENABLED
+        if (jthrowable e = env->ExceptionOccurred()) {
+            jclass string_writer_class {env->FindClass("java/io/StringWriter")};
+            jmethodID string_writer_constructor {env->GetMethodID(string_writer_class, "<init>", "()V")};
+            jobject string_writer {env->NewObject(string_writer_class, string_writer_constructor)};
+
+            jclass print_writer_class {env->FindClass("java/io/PrintWriter")};
+            jmethodID print_writer_constructor {env->GetMethodID(print_writer_class, "<init>", "(Ljava/io/Writer;)V")};
+            jobject print_writer {env->NewObject(print_writer_class, print_writer_constructor, string_writer)};
+
+            jclass throwable_class {env->FindClass("java/lang/Throwable")};
+            jmethodID print_stack_trace_method {env->GetMethodID(throwable_class, "printStackTrace", "(Ljava/io/PrintWriter;)V")};
+            env->CallVoidMethod(e, print_stack_trace_method, print_writer);
+
+            jmethodID to_string_method {env->GetMethodID(string_writer_class, "toString", "()Ljava/lang/String;")};
+            auto j_stack_trace_string {(jstring) env->CallObjectMethod(string_writer, to_string_method)};
+
+            const char* c_stack_trace {env->GetStringUTFChars(j_stack_trace_string, nullptr)};
+            String stack_trace {c_stack_trace};
+            env->ReleaseStringUTFChars(j_stack_trace_string, c_stack_trace);
+
+            return stack_trace;
+        }
+#else
         env->ExceptionDescribe();
+#endif
+        return {};
     }
 
     void Env::exception_clear() {
@@ -48,9 +74,13 @@ namespace jni {
 
     void Env::check_exceptions() {
         if (exception_check()) {
-            exception_describe();
+            String exception {exception_describe()};
             exception_clear();
-            HANDLE_JVM_EXCEPTIONS(true, "An exception has occurred!");
+
+            if (exception.is_empty()) {
+                exception = "An exception has occurred!";
+            }
+            HANDLE_JVM_EXCEPTIONS(true, exception);
         }
     }
 
--- src/jni/env.h
+++ src/jni/env.h
@@ -45,7 +45,7 @@ namespace jni {
         String from_jstring(JString str);
 
         bool exception_check();
-        void exception_describe();
+        String exception_describe();
         void exception_clear();
 
         void check_exceptions();
