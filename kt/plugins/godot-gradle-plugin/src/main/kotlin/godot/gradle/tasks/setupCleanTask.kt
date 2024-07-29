32b03a2adc9db9bb3dc093a127096c59df3d953a
chippmann <cedric.hippmann@hotmail.com>
2024-07-29 09:37:33 +0200

    Cleanup task structure and implement copy task to copy built jars to `jvm` folder

--- kt/plugins/godot-gradle-plugin/src/main/kotlin/godot/gradle/tasks/setupCleanTask.kt
+++ kt/plugins/godot-gradle-plugin/src/main/kotlin/godot/gradle/tasks/setupCleanTask.kt
@@ -5,14 +5,11 @@ import org.gradle.api.Task
 import org.gradle.api.tasks.TaskProvider
 
 fun Project.setupCleanTask(
-    createBuildLockTask: TaskProvider<out Task>,
-    deleteBuildLockTask: TaskProvider<out Task>,
     generateGdIgnoreFilesTask: TaskProvider<out Task>
 ) {
     tasks.named("clean") {
         with(it) {
-            dependsOn(createBuildLockTask)
-            finalizedBy(deleteBuildLockTask, generateGdIgnoreFilesTask)
+            finalizedBy(generateGdIgnoreFilesTask)
         }
     }
 }
