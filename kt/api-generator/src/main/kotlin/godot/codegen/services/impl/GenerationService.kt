590edb1f345938be47c81778c2ac7157652df8d4
Pierre-Thomas Meisels <meisels27@yahoo.fr>
2024-08-08 19:52:38 +0200

    feat: Implement KotlinCallableCustom to enable lambda callables

--- kt/api-generator/src/main/kotlin/godot/codegen/services/impl/GenerationService.kt
+++ kt/api-generator/src/main/kotlin/godot/codegen/services/impl/GenerationService.kt
@@ -548,7 +548,7 @@ class GenerationService(
                     }
 
                     appendLine(
-                        """This is a helper function to make dealing with local copies easier. 
+                        """This is a helper function to make dealing with local copies easier.
                     |
                     |For more information, see our [documentation](https://godot-kotl.in/en/stable/user-guide/api-differences/#core-types).
                     |
