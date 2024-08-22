4b9f37643306283a0cd7fcb6e8c35336dcfe6f99
chippmann <cedric.hippmann@hotmail.com>
2024-08-22 21:14:05 +0200

    Improve editor executable selection

--- harness/tests/build.gradle.kts
+++ harness/tests/build.gradle.kts
@@ -208,5 +208,5 @@ fun provideEditorExecutable(): File = (projectDir
     ?.also {
         println("[${it.joinToString()}]")
     }
-    ?.firstOrNull { it.name.startsWith("godot.") }
+    ?.firstOrNull { it.name.startsWith("godot.") && it.name.contains("editor") }
     ?: throw Exception("Could not find editor executable"))
