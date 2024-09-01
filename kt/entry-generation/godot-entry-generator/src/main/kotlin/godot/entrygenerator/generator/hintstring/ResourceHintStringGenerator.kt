a37b893df4e4a083d336b8971dfd612b27de2acc
Cedric Hippmann <cedric.hippmann@hotmail.com>
2024-09-01 10:29:43 +0200

    Fix custom classes type name (#678)

--- kt/entry-generation/godot-entry-generator/src/main/kotlin/godot/entrygenerator/generator/hintstring/ResourceHintStringGenerator.kt
+++ kt/entry-generation/godot-entry-generator/src/main/kotlin/godot/entrygenerator/generator/hintstring/ResourceHintStringGenerator.kt
@@ -1,5 +1,7 @@
 package godot.entrygenerator.generator.hintstring
 
+import godot.entrygenerator.EntryGenerator
+import godot.entrygenerator.ext.baseGodotType
 import godot.entrygenerator.model.PropertyHintAnnotation
 import godot.entrygenerator.model.RegisteredProperty
 
@@ -7,5 +9,9 @@ class ResourceHintStringGenerator(
     registeredProperty: RegisteredProperty
 ) : PropertyHintStringGenerator<PropertyHintAnnotation>(registeredProperty) {
 
-    override fun getHintString(): String = registeredProperty.type.fqName.substringAfterLast(".")
+    override fun getHintString(): String {
+        // we first try to use the registered class name in case it's a user type extending a godot type
+        // if that is not the case (null) we get the simple name instead
+        return registeredProperty.type.registeredName() ?: registeredProperty.type.fqName.substringAfterLast(".")
+    }
 }
