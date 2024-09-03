6000c843eabc29cdf0583b93bf42be1ad3980bfb
Ced Naru <cednaru.dev@gmail.com>
2024-09-03 06:13:29 +0200

    Improve MemoryManager smoothing

--- kt/godot-library/src/main/kotlin/godot/core/memory/MemoryManager.kt
+++ kt/godot-library/src/main/kotlin/godot/core/memory/MemoryManager.kt
@@ -16,20 +16,49 @@ import java.util.concurrent.locks.ReentrantReadWriteLock
 import kotlin.concurrent.read
 import kotlin.concurrent.write
 import kotlin.math.max
-import kotlin.math.min
+import kotlin.math.roundToInt
 
 internal object MemoryManager {
+
+    /** With those constants, the manager can handle up to 30_000 items at once in the allowed time.
+     *  You can compute the number of iterations it takes to process everything with the following formula:
+     * ```
+     * -(MAX_ITERATION / DECAY_FACTOR) × (
+     *     ln(
+     *         (MAX_GC_NUMBER × MAX_ITERATION) / (size × DECAY_FACTOR)
+     *     ) - 1
+     * )
+     * ```
+     * */
+    private object Configuration {
+        /** Number of references to decrement each loop on a light load (Doesn't have priority over the ratio).*/
+        private const val MAX_GC_NUMBER = 64f
+
+        /** The maximum amount of iteration it should take for a binding to be processed by the Manager in case of heavy load.*/
+        private const val MAX_ITERATION = 60f
+
+        /** The processing of queues follows an exponential decay. This number is the result of `ln(0.01)`.
+         * It means that 99% of the queue will be processed by the time the max number of iterations is reached.
+         */
+        private const val DECAY_FACTOR = 4.6051702f
+
+        /**
+         * The only variable available is the current size of the queue. We want to make sure everything is processed in time.
+         * We can't simply process `1f/MAX_ITERATION` items when under load. Because of the exponential decay, it would take much longer than `MAX_ITERATION`.
+         * We increase the ratio enough that 99% of the queue can be processed in the allowed time.
+         * In practice when the number of items in the queue becomes small enough, `MAX_GC_NUMBER` will take over and handle the remaining 1% in time.
+         * This is a finite approximation of the function ```f(x) = size * exp(-x * DECAY_FACTOR / MAX_ITERATION)```
+         */
+        fun getNumberOfItemsToProcess(size: Int) = max(MAX_GC_NUMBER, size * (DECAY_FACTOR / MAX_ITERATION))
+            .roundToInt()
+            .coerceAtMost(size)
+    }
+
     /** Base capacity for caches.*/
     private const val CACHE_INITIAL_CAPACITY = 256
 
     /** Base capacity for several binding containers.*/
     private const val BINDING_INITIAL_CAPACITY = 4096
-
-    /** Number of references to decrement each loop at most (Doesn't have priority over the ratio).*/
-    private const val MAX_GC_NUMBER = 64
-
-    /** The fraction of references to check each loop at least (chosen so everything is freed after 1 second at most, assuming a 60 fps game).*/
-    private const val MIN_GC_RATIO = 1f / 60f
 
     private val lock = ReentrantReadWriteLock()
 
@@ -194,12 +223,7 @@ internal object MemoryManager {
             deadReferences.add(ref)
         }
 
-        val size = deadReferences.size
-        val numberToDecrement = max(
-            min(MAX_GC_NUMBER, size),
-            (MIN_GC_RATIO * size).toInt()
-        )
-
+        val numberToDecrement = Configuration.getNumberOfItemsToProcess(deadReferences.size)
         val sublist = deadReferences.subList(0, numberToDecrement)
 
         val deadArray = lock.write {
@@ -248,12 +272,7 @@ internal object MemoryManager {
             deadNativeCores.add(ref)
         }
 
-        val size = deadNativeCores.size
-        val numberToDecrement = max(
-            min(MAX_GC_NUMBER, size),
-            (MIN_GC_RATIO * size).toInt()
-        )
-
+        val numberToDecrement = Configuration.getNumberOfItemsToProcess(deadNativeCores.size)
         val pointerArray = LongArray(numberToDecrement)
         val variantTypeArray = IntArray(numberToDecrement)
 
