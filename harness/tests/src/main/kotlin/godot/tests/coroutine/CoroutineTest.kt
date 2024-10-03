0a1a5752abf03916fb8743ff889162f41a75e03c
Ced Naru <cednaru.dev@gmail.com>
2024-10-03 15:44:38 +0200

    Update tests

--- harness/tests/src/main/kotlin/godot/tests/coroutine/CoroutineTest.kt
+++ harness/tests/src/main/kotlin/godot/tests/coroutine/CoroutineTest.kt
@@ -12,10 +12,10 @@ import godot.core.Vector2
 import godot.core.signal0
 import godot.core.signal1
 import godot.core.signal4
-import godot.coroutines.GodotCoroutine
+import godot.coroutines.godotCoroutine
 import godot.coroutines.await
-import godot.coroutines.awaitDeferred
 import godot.coroutines.awaitLoadAs
+import godot.coroutines.awaitMainThread
 import godot.global.GD
 import kotlinx.coroutines.CoroutineStart
 
@@ -35,27 +35,27 @@ class CoroutineTest : Object() {
     var step: Int = 0
 
     @RegisterFunction
-    fun startCoroutineWithoutParameter() = GodotCoroutine {
+    fun startCoroutineWithoutParameter() = godotCoroutine {
         step = 1
         signalWithoutParameter.await()
         step = 2
     }
 
     @RegisterFunction
-    fun startCoroutineWithOneParameter() = GodotCoroutine {
+    fun startCoroutineWithOneParameter() = godotCoroutine {
         step = 3
         step = signalWithOneParameter.await()
     }
 
     @RegisterFunction
-    fun startCoroutineWithManyParameters() = GodotCoroutine {
+    fun startCoroutineWithManyParameters() = godotCoroutine {
         step = 5
         val (int, _, _, _) = signalWithManyParameters.await()
         step = int
     }
 
     @RegisterFunction
-    fun startCoroutineUndispatched() = GodotCoroutine(start = CoroutineStart.UNDISPATCHED) {
+    fun startCoroutineUndispatched() = godotCoroutine(start = CoroutineStart.UNDISPATCHED) {
         step = 7
         signalWithoutParameter.await()
         step = 8
@@ -68,11 +68,11 @@ class CoroutineTest : Object() {
     fun runOnMainThreadFromBackgroundThread() {
         val thread = Thread.currentThread().name
 
-        GodotCoroutine {
+        godotCoroutine {
             val bgThread = Thread.currentThread().name
 
             var fgThread = ""
-            awaitDeferred {
+            awaitMainThread {
                 fgThread = Thread.currentThread().name
             }
 
@@ -80,7 +80,7 @@ class CoroutineTest : Object() {
 
             GD.print("Thread names: ${listOf(thread, bgThread, fgThread, bg2Thread).joinToString()}")
 
-            awaitDeferred {
+            awaitMainThread {
                 runOnMainThreadFromBackgroundThreadFinished.emit(
                     thread != bgThread
                         && thread != bg2Thread
@@ -96,14 +96,14 @@ class CoroutineTest : Object() {
 
     @RegisterFunction
     fun asyncLoadResource() {
-        GodotCoroutine {
+        godotCoroutine {
             val resource = ResourceLoader.awaitLoadAs<PackedScene>("res://Spatial.tscn") { progress ->
                 GD.print("Resource load progress: $progress")
             }
 
             GD.print("Resource: $resource")
 
-            awaitDeferred {
+            awaitMainThread {
                 asyncLoadResourceFinished.emit(resource != null)
             }
         }
