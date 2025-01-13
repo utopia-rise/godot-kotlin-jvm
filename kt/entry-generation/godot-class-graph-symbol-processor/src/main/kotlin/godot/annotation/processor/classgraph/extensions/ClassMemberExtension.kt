441caabb76b9504c39a99622f2cf8b9bbf6b4840
Pierre-Thomas Meisels <meisels27@yahoo.fr>
2025-01-13 18:03:55 +0100

    feat(entry-gen): Manage type parameters with classgraph

--- kt/entry-generation/godot-class-graph-symbol-processor/src/main/kotlin/godot/annotation/processor/classgraph/extensions/ClassMemberExtension.kt
+++ kt/entry-generation/godot-class-graph-symbol-processor/src/main/kotlin/godot/annotation/processor/classgraph/extensions/ClassMemberExtension.kt
@@ -2,8 +2,10 @@ package godot.annotation.processor.classgraph.extensions
 
 import io.github.classgraph.ClassMemberInfo
 
+const val DELEGATE_SUFFIX = "\$delegate"
+
 val ClassMemberInfo.fqdn: String
     get() = "${classInfo.name}\$$sanitizedName"
 
 val ClassMemberInfo.sanitizedName: String
-    get() = name.removeSuffix("\$delegate")
+    get() = name.removeSuffix(DELEGATE_SUFFIX)
