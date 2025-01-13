532309c88b07127aa268f7b00a0762017b1c24a6
Pierre-Thomas Meisels <meisels27@yahoo.fr>
2025-01-13 18:03:55 +0100

    feat(entry-gen): Manage type parameters with classgraph

---
+++ kt/entry-generation/godot-class-graph-symbol-processor/src/main/kotlin/godot/annotation/processor/classgraph/constants/Types.kt
@@ -0,0 +1,27 @@
+package godot.annotation.processor.classgraph.constants
+
+const val BOOLEAN = "boolean"
+const val INT = "int"
+const val LONG = "long"
+const val FLOAT = "float"
+const val DOUBLE = "double"
+const val BYTE = "byte"
+const val SHORT = "short"
+
+val jvmPrimitives = listOf(
+    BOOLEAN,
+    INT,
+    LONG,
+    FLOAT,
+    DOUBLE,
+    BYTE,
+    SHORT
+)
+
+const val VOID = "void"
+
+const val STRING = "java.lang.String"
+
+const val JVM_OBJECT = "java.lang.Object"
+
+const val SET = "java.util.Set"
