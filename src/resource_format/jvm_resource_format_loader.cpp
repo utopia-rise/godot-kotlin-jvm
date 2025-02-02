7d4e50c930eb00b8e2c9c9828aea35c6ab4602df
Pierre-Thomas Meisels <meisels27@yahoo.fr>
2025-02-02 13:12:15 +0100

    feat: remove script source file path from KtClass and registration.

--- src/resource_format/jvm_resource_format_loader.cpp
+++ src/resource_format/jvm_resource_format_loader.cpp
@@ -31,8 +31,7 @@ bool JvmResourceFormatLoader::handles_type(const String& p_type) const {
     return p_type == "Script" || p_type == GODOT_KOTLIN_SCRIPT_NAME || p_type == GODOT_JVM_SCRIPT_NAME || p_type == GODOT_JAVA_SCRIPT_NAME;
 }
 
-#ifdef TOOLS_ENABLED
-Error read_all_file_utf8(const String& p_path, String& r_content) {
+Error JvmResourceFormatLoader::read_all_file_utf8(const String& p_path, String& r_content) {
     Vector<uint8_t> source_file;
     Error err;
     Ref<FileAccess> file_access {FileAccess::open(p_path, FileAccess::READ, &err)};
@@ -51,7 +50,6 @@ Error read_all_file_utf8(const String& p_path, String& r_content) {
     r_content = source;
     return OK;
 }
-#endif
 
 Ref<Resource> JvmResourceFormatLoader::load(const String& p_path, const String& p_original_path, Error* r_error, bool p_use_sub_threads, float* r_progress, CacheMode p_cache_mode) {
     Ref<JvmScript> jvm_script;
@@ -60,13 +58,13 @@ Ref<Resource> JvmResourceFormatLoader::load(const String& p_path, const String& 
     bool script_is_new = true;
     bool is_source;
     if (extension == GODOT_JVM_REGISTRATION_FILE_EXTENSION) {
-        jvm_script = JvmScriptManager::get_instance()->get_or_create_script<GdjScript>(p_path, &script_is_new);
+        jvm_script = JvmScriptManager::get_instance()->get_or_create_named_script<GdjScript>(p_path, &script_is_new);
         is_source = false;
     } else if (extension == GODOT_KOTLIN_SCRIPT_EXTENSION) {
-        jvm_script = JvmScriptManager::get_instance()->get_or_create_script<KotlinScript>(p_path, &script_is_new);
+        jvm_script = JvmScriptManager::get_instance()->get_or_create_source_script<KotlinScript>(p_path, &script_is_new, r_error);
         is_source = true;
     } else if (extension == GODOT_JAVA_SCRIPT_EXTENSION) {
-        jvm_script = JvmScriptManager::get_instance()->get_or_create_script<JavaScript>(p_path, &script_is_new);
+        jvm_script = JvmScriptManager::get_instance()->get_or_create_source_script<JavaScript>(p_path, &script_is_new, r_error);
         is_source = true;
     } else {
         if (r_error) { *r_error = Error::ERR_FILE_UNRECOGNIZED; }
@@ -75,19 +73,15 @@ Ref<Resource> JvmResourceFormatLoader::load(const String& p_path, const String& 
 
     if (jvm_script.is_valid()) {
 #ifdef TOOLS_ENABLED
-        String source_code;
-        Error load_err {read_all_file_utf8(p_path, source_code)};
-        if (r_error) { *r_error = load_err; }
-        jvm_script->set_source_code(source_code);
 
+        //TODO: Remove
         if (is_source) {
             JVM_LOG_INFO(vformat("fqdn: %s", dynamic_cast<SourceScript*>(jvm_script.ptr())->get_functional_name()));
         }
 
         if (!script_is_new && is_source) {
-            String path = jvm_script->get_path();
             MessageQueue::get_singleton()->push_callable(
-              callable_mp(JvmScriptManager::get_instance(), &JvmScriptManager::invalidate_source).bind(path)
+              callable_mp(JvmScriptManager::get_instance(), &JvmScriptManager::invalidate_source).bind(Ref<SourceScript>(jvm_script))
             );
         }
 #endif
