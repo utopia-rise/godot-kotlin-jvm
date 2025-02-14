8e10ffc21ceabd940e2835c4bf8deb0ecf245e02
Pierre-Thomas Meisels <meisels27@yahoo.fr>
2025-02-14 11:54:15 +0100

    fix(ci): make install vulkan script to not depend on version

--- install_vulkan_sdk_macos.sh
+++ install_vulkan_sdk_macos.sh
@@ -5,12 +5,15 @@ IFS=$'\n\t'
 
 # Download and install the Vulkan SDK.
 curl -L "https://sdk.lunarg.com/sdk/download/latest/mac/vulkan-sdk.zip" -o /tmp/vulkan-sdk.zip
+unzip -l /tmp/vulkan-sdk.zip | grep 'InstallVulkan.*\.app' | head -n 1 | awk '{print $4}' | tr -d '/' > /tmp/install_app_name
+
 unzip /tmp/vulkan-sdk.zip -d /tmp
-/tmp/InstallVulkan.app/Contents/MacOS/InstallVulkan \
+install_app_name=$(cat /tmp/install_app_name)
+/tmp/$install_app_name/Contents/MacOS/${install_app_name%\.app} \
     --accept-licenses --default-answer --confirm-command install
 
-
-rm -rf /tmp/InstallVulkan.app
+rm -rf /tmp/$install_app_name
 rm -f /tmp/vulkan-sdk.zip
+rm -f /tmp/install_app_name
 
 echo 'Vulkan SDK installed successfully! You can now build Godot by running "scons".'
