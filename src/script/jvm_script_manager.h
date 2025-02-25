314a55ef055e647aff1e3541f4fe2f922983aa9f
Ced Naru <cednaru.dev@gmail.com>
2025-02-25 19:19:21 +0100

    Remove lazy for named scripts

--- src/script/jvm_script_manager.h
+++ src/script/jvm_script_manager.h
@@ -8,8 +8,8 @@
 class JvmScriptManager: public Object {
     friend class Memory;
 
-    Vector<Ref<WeakRef>> named_scripts;
-    HashMap<StringName, Ref<WeakRef>> named_scripts_map;
+    Vector<Ref<NamedScript>> named_scripts;
+    HashMap<StringName, Ref<NamedScript>> named_scripts_map;
 
     HashMap<String, StringName> fqdn_to_name_map;
     HashMap<StringName, String> name_to_fqdn_map;
@@ -17,7 +17,6 @@ class JvmScriptManager: public Object {
     Vector<Ref<WeakRef>> source_scripts;
     HashMap<StringName, Ref<WeakRef>> source_scripts_map;
 
-    HashMap<StringName, KtClass*> name_to_kt_class;
     HashMap<StringName, KtClass*> fqdn_to_kt_class;
 
 #ifdef TOOLS_ENABLED
@@ -43,7 +42,6 @@ public:
     Ref<NamedScript> get_named_script_from_index(int p_index) const;
     Ref<NamedScript> get_named_script_from_source_script(Ref<SourceScript> p_source_script) const;
     Ref<SourceScript> get_script_from_fqdn(const StringName& p_fqdn) const;
-    KtClass* get_kt_class_from_name(const String& p_name) const;
 
     template<class SCRIPT>
     Ref<SCRIPT> get_or_create_named_script(const String& p_path, bool* created);
@@ -61,29 +59,19 @@ public:
 
 template<class SCRIPT>
 Ref<SCRIPT> JvmScriptManager::get_or_create_named_script(const String& p_path, bool* created) {
-    if constexpr (!std::is_base_of<JvmScript, SCRIPT>()) { return {}; }
+    if constexpr (!std::is_base_of<NamedScript, SCRIPT>()) { return {}; }
     // Placeholder scripts have to be registered in the TypeManager in order to be transformed in valid scripts when the jar is built.
 
     *created = false;
-    // If a named script weak ref is already created and valid, we return it, otherwise we create the script
+    Ref<SCRIPT> jvm_script;
+    // Named scripts are created and cached when loading the usercode, we create a placeholder if missing.
     String script_name = JvmScript::get_script_file_name(p_path);
-    Ref<SCRIPT> jvm_script = get_script_from_name(script_name);
+    jvm_script = get_script_from_name(script_name);
     if (jvm_script.is_null()) {
         jvm_script.instantiate();
-        jvm_script->kotlin_class = name_to_kt_class[script_name];
         *created = true;
-
-        //TODO: Move to resource format loader
-        String source;
-        JvmResourceFormatLoader::read_all_file_utf8(p_path, source);
-        jvm_script->set_source_code(source);
-
-        Ref<WeakRef> weak_ref;
-        weak_ref.instantiate();
-        weak_ref->set_ref(jvm_script);
-
-        named_scripts_map[script_name] = weak_ref;
-        named_scripts.push_back(weak_ref);
+        named_scripts_map[script_name] = jvm_script;
+        named_scripts.push_back(jvm_script);
     }
     return jvm_script;
 }
