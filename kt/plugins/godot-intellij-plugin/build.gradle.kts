d258a56d659d199a2d056b51ec0bdc863f79ad04
chippmann <cedric@hippmann.ch>
2025-02-26 17:03:11 +0100

    Fix plugin not compatible and older plugin version suggested
    See: #755

--- kt/plugins/godot-intellij-plugin/build.gradle.kts
+++ kt/plugins/godot-intellij-plugin/build.gradle.kts
@@ -73,6 +73,9 @@ tasks {
             this.pluginVersion.set("${project.version}-IJ$intellijVersion")
         }
         sinceBuild.set("242.3")
+        // magic values like 999.* are no longer supported. But we can support current version +2 years. Example: current 242.3 (2024.2.3) until 279.* (2027.9.*)
+        // this prevents us from needing to update the ide plugin with every ide version update and gives us ~2 years time for that after the last release
+        untilBuild.set("279.*")
 
         // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
         pluginDescription.set(
