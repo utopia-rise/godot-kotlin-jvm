c11c6621c83455faaf92f2dd20049fc5d464e9e1
chippmann <cedric@hippmann.ch>
2025-02-26 18:02:26 +0100

    Update foojay resolver convention to `0.9.0`

--- kt/tools-common/settings.gradle.kts
+++ kt/tools-common/settings.gradle.kts
@@ -26,5 +26,5 @@ dependencyResolutionManagement {
 
 plugins {
     // to automatically download the toolchain jdk if missing
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.6.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0" // https://plugins.gradle.org/plugin/org.gradle.toolchains.foojay-resolver-convention
 }
