280d8e9b2303a547e195f800539ba6de13392df6
Ced Naru <cednaru.dev@gmail.com>
2025-03-15 22:46:38 +0100

    Move Documentation sanitize to a rule

--- kt/api-generator/src/main/kotlin/godot/codegen/generation/rule/FileRule.kt
+++ kt/api-generator/src/main/kotlin/godot/codegen/generation/rule/FileRule.kt
@@ -3,11 +3,13 @@ package godot.codegen.generation.rule
 import com.squareup.kotlinpoet.AnnotationSpec
 import com.squareup.kotlinpoet.ClassName
 import godot.codegen.generation.Context
+import godot.codegen.generation.task.ApiTask
 import godot.codegen.generation.task.EnrichedClassTask
 import godot.codegen.generation.task.EnrichedEnumTask
 import godot.codegen.generation.task.FileTask
 import godot.codegen.models.enriched.EnrichedClass
 import godot.codegen.models.enriched.EnrichedEnum
+import godot.common.extensions.convertToCamelCase
 import godot.tools.common.constants.GENERATED_COMMENT
 
 class FileRule : GodotApiRule<FileTask>() {
@@ -69,5 +71,103 @@ class StaticRule : GodotApiRule<FileTask>() {
                 }
             }
         }
+    }
+}
+
+private const val constantTitle = "constant"
+
+class DocumentationRule : GodotApiRule<ApiTask>() {
+    private val titlesToSanitize = arrayOf(
+        "member",
+        "method",
+        "enum",
+        constantTitle,
+        "param"
+    )
+
+    private val tagsToSanitize = arrayOf(
+        "code" to "`",
+        "i" to "*",
+        "b" to "**"
+    )
+
+    private val languages = arrayOf(
+        "gdscript",
+        "csharp"
+    )
+
+    override fun apply(fileTask: ApiTask, context: Context) {
+        val enumValues = context
+            .enumRepository
+            .getGlobalEnums()
+            .flatMap { it.values }
+
+        val classes = context.classRepository.listTypes()
+        val members = classes.flatMap { it.methods + it.properties + it.constants + it.signals }
+        val innerEnumValue = classes.flatMap { it.enums }.flatMap { it.values }
+
+        val allDocumented = enumValues + classes + members + innerEnumValue
+
+        allDocumented.forEach {
+            it.description = sanitize(it.description)
+        }
+    }
+
+    private fun sanitize(documentation: String?): String {
+        if (documentation.isNullOrEmpty()) {
+            return ""
+        }
+
+        var unicodeString = documentation
+            .replace("/*", "&#47;*")
+            .replace("%", "&#37;")
+            .replace("*/", "*&#92;")
+            .replace(System.lineSeparator(), "\n")
+
+        for (title in titlesToSanitize) {
+            val regex = Regex("\\[($title\\s)(\\S+)(\\])")
+
+            var matchResult = unicodeString.let { regex.find(it) }
+            while (matchResult != null) {
+                val titleRange = matchResult.groups[1]!!.range
+                val contentRange = matchResult.groups[2]!!.range
+                val content = with(unicodeString.substring(contentRange)) {
+                    if (unicodeString.substring(titleRange).startsWith(constantTitle)) {
+                        return@with this
+                    }
+                    convertToCamelCase()
+                }
+                unicodeString = unicodeString
+                    .replaceRange(contentRange, content)
+                    .removeRange(titleRange)
+                matchResult = unicodeString.let { regex.find(it) }
+            }
+        }
+
+        for (tag in tagsToSanitize) {
+            val regex = Regex("(\\[${tag.first}\\])((?:(?!\\[\\/${tag.first}\\]|\\[${tag.first}\\]).)*)(\\[\\/${tag.first}\\])")
+
+            var matchResult = unicodeString.let { regex.find(it) }
+            while (matchResult != null) {
+                val endTagRange = matchResult.groups[3]!!.range
+                val beginTagRange = matchResult.groups[1]!!.range
+                unicodeString = unicodeString
+                    .replaceRange(endTagRange, tag.second)
+                    .replaceRange(beginTagRange, tag.second)
+                matchResult = unicodeString.let { regex.find(it) }
+            }
+        }
+
+        unicodeString = unicodeString
+            .replace("[codeblocks]", "")
+            .replace("[/codeblocks]", "")
+
+        for (language in languages) {
+            unicodeString = unicodeString
+                .replace("[$language]", "$language:\n```$language")
+                .replace("[/$language]", "```")
+        }
+
+        return unicodeString
     }
 }
