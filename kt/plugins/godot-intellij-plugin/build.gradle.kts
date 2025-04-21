844fc5769cf5475f4d2532029e7f91b2fd5259e4
Cedric Hippmann <cedric@hippmann.ch>
2025-04-21 15:59:23 +0200

    Update ide plugin for 2025.1 (#818)
    * Update ide plugin
    
    * Bump until version as well
    
    * Add clarification util when the current version will be supported

--- kt/plugins/godot-intellij-plugin/build.gradle.kts
+++ kt/plugins/godot-intellij-plugin/build.gradle.kts
@@ -74,10 +74,10 @@ tasks {
         } else {
             this.pluginVersion.set("${project.version}-IJ$intellijVersion")
         }
-        sinceBuild.set("243.1")
+        sinceBuild.set("251.1")
         // magic values like 999.* are no longer supported. But we can support current version +2 years. Example: current 242.3 (2024.2.3) until 279.* (2027.9.*)
         // this prevents us from needing to update the ide plugin with every ide version update and gives us ~2 years time for that after the last release
-        untilBuild.set("279.*")
+        untilBuild.set("289.*") // until the end of 2028
 
         // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
         pluginDescription.set(
