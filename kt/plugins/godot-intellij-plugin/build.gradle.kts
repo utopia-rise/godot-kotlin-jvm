ec688a936e8a1c2d086c4f7ba15167d01ba3ff6e
chippmann <cedric@hippmann.ch>
2025-04-21 14:38:46 +0200

    Bump until version as well

--- kt/plugins/godot-intellij-plugin/build.gradle.kts
+++ kt/plugins/godot-intellij-plugin/build.gradle.kts
@@ -77,7 +77,7 @@ tasks {
         sinceBuild.set("251.1")
         // magic values like 999.* are no longer supported. But we can support current version +2 years. Example: current 242.3 (2024.2.3) until 279.* (2027.9.*)
         // this prevents us from needing to update the ide plugin with every ide version update and gives us ~2 years time for that after the last release
-        untilBuild.set("279.*")
+        untilBuild.set("289.*")
 
         // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
         pluginDescription.set(
