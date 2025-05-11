5eeb7d6a2a77aae430bd1f41eef436d53fe60c5e
Tristan Grespinet <cednaru.dev@gmail.com>
2025-05-11 22:05:00 +0200

    Improve Custom Callables and add SignalConnector (#803)

--- kt/godot-library/godot-core-library/src/main/kotlin/gen/godot/api/Object.kt
+++ kt/godot-library/godot-core-library/src/main/kotlin/gen/godot/api/Object.kt
@@ -852,6 +852,7 @@ public open class Object : KtObject() {
     flags: ConnectFlags = Object.ConnectFlags.DEFAULT,
   ): Error {
     TransferContext.writeArguments(STRING_NAME to signal, CALLABLE to callable, LONG to flags.value)
+    TransferContext.writeArguments(STRING_NAME to signal, CALLABLE to callable, LONG to flags.id)
     TransferContext.callMethod(ptr, MethodBindings.connectPtr, LONG)
     return Error.from(TransferContext.readReturnValue(LONG) as Long)
   }
@@ -1533,6 +1534,10 @@ public open class Object : KtObject() {
   public enum class ConnectFlags(
     `value`: Long,
   ) : GodotEnum {
+    /**
+     * Default connections that are immediately emitted
+     */
+    DEFAULT(0),
     /**
      * Default connections that are immediately emitted
      */
