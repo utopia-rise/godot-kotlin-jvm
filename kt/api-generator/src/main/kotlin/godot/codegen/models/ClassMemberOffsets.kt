7cfe746d78c165cd4128146466cb71dbba35f35e
chippmann <cedric@hippmann.ch>
2025-08-08 14:32:26 +0200

    Update dependencies

--- kt/api-generator/src/main/kotlin/godot/codegen/models/ClassMemberOffsets.kt
+++ kt/api-generator/src/main/kotlin/godot/codegen/models/ClassMemberOffsets.kt
@@ -3,6 +3,6 @@ package godot.codegen.models
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class ClassMemberOffsets(
-    @JsonProperty("name") val name: String,
-    @JsonProperty("members") val members: List<MemberOffset>
+    @param:JsonProperty("name") val name: String,
+    @param:JsonProperty("members") val members: List<MemberOffset>
 )
--- kt/api-generator/src/main/kotlin/godot/codegen/models/UtilityFunction.kt
+++ kt/api-generator/src/main/kotlin/godot/codegen/models/UtilityFunction.kt
@@ -4,12 +4,12 @@ import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class UtilityFunction @JsonCreator constructor(
-    @JsonProperty("name") val name: String,
-    @JsonProperty("return_type") val returnType: String?,
-    @JsonProperty("category") val category: String,
-    @JsonProperty("is_vararg") val isVararg: Boolean,
-    @JsonProperty("hash") val hash: Long,
-    @JsonProperty("arguments") val arguments: List<Argument>?,
-    @JsonProperty("description") val description: String?,
-    @JsonProperty("brief_description") val briefDescription: String?
+    @param:JsonProperty("name") val name: String,
+    @param:JsonProperty("return_type") val returnType: String?,
+    @param:JsonProperty("category") val category: String,
+    @param:JsonProperty("is_vararg") val isVararg: Boolean,
+    @param:JsonProperty("hash") val hash: Long,
+    @param:JsonProperty("arguments") val arguments: List<Argument>?,
+    @param:JsonProperty("description") val description: String?,
+    @param:JsonProperty("brief_description") val briefDescription: String?
 )
