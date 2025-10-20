b6bedaa43df5a5c9a021ae5be3dd23cbf8e6c3c8
Pierre-Thomas Meisels <meisels27@yahoo.fr>
2025-10-20 19:04:18 +0200

    fix: replace $ in typenames in classgraph symbol processor

--- harness/tests/test/unit/test_constructors.gd
+++ harness/tests/test/unit/test_constructors.gd
@@ -2,16 +2,6 @@ extends "res://addons/gut/test.gd"
 
 
 func test_should_call_default_constructor() -> void:
-	var test_instance = MultiArgsConstructorTest.new()
-	assert_true(test_instance.default_constructor_has_been_called, "Default constructor should have been called")
-	test_instance.free()
-
-func test_should_call_one_arg_constructor() -> void:
-	var test_instance = load("res://scripts/godot/tests/MultiArgsConstructorTest.gdj").new(42)
-	assert_true(test_instance.one_arg_constructor_has_been_called, "One arg constructor should have been called")
-	test_instance.free()
-
-func test_should_call_three_args_constructor() -> void:
-	var test_instance = load("res://scripts/godot/tests/MultiArgsConstructorTest.gdj").new(42, "hello")
-	assert_true(test_instance.three_args_constructor_has_been_called, "Three args constructor should have been called")
-	test_instance.free()
+    var test_instance = ConstructorRegistrationTest.new()
+    assert_true(test_instance.default_constructor_has_been_called, "Default constructor should have been called")
+    test_instance.free()
