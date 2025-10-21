a1cfd4c68ce57fe3e28ec749ac68f6c2596f7835
Pierre-Thomas Meisels <meisels27@yahoo.fr>
2025-10-21 03:50:19 +0200

    fix: replace $ in typenames in classgraph symbol processor (#879)

--- harness/tests/src/main/kotlin/godot/tests/constructor/ConstructorRegistrationTest.kt
+++ harness/tests/src/main/kotlin/godot/tests/constructor/ConstructorRegistrationTest.kt
@@ -2,6 +2,14 @@ package godot.tests.constructor
 
 import godot.api.Node
 import godot.annotation.RegisterClass
+import godot.annotation.RegisterProperty
 
 @RegisterClass
-class ConstructorRegistrationTest(): Node()
+class ConstructorRegistrationTest(): Node() {
+    @RegisterProperty
+    var defaultConstructorHasBeenCalled = false
+
+    init {
+        defaultConstructorHasBeenCalled = true
+    }
+}
