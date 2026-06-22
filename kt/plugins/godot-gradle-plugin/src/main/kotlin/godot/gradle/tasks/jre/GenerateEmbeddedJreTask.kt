75a784ff32ccf18f018c670bb4570c900fdd0e97
CedNaru <cednaru.dev@gmail.com>
2026-06-23 00:24:51 +0200

    Fix JRE embedding

--- kt/plugins/godot-gradle-plugin/src/main/kotlin/godot/gradle/tasks/jre/GenerateEmbeddedJreTask.kt
+++ kt/plugins/godot-gradle-plugin/src/main/kotlin/godot/gradle/tasks/jre/GenerateEmbeddedJreTask.kt
@@ -3,6 +3,7 @@ package godot.gradle.tasks
 import godot.tools.common.constants.Paths
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
 import org.gradle.process.ExecOperations
 import java.io.File
@@ -22,6 +23,9 @@ open class GenerateEmbeddedJreTask @Inject constructor(
     @Input
     var outputDir: String = "${Paths.GODOT_KOTLIN_JVM_DIR}/jre-${getArch()}-${getOs()}"
 
+    @OutputDirectory
+    fun getJreOutputDirectory(): File = File(outputDir)
+
     @Input
     var arguments: Array<String> = arrayOf(
         "--strip-debug",
@@ -30,17 +34,23 @@ open class GenerateEmbeddedJreTask @Inject constructor(
     )
 
     @Input
-    var javaHome: String? = null
+    var javaHome: String = System.getenv("JAVA_HOME") ?: System.getProperty("java.home")
 
     @TaskAction
     fun createJre() {
         File(outputDir).deleteRecursively()
 
-        val resolvedJavaHome = javaHome ?: System.getProperty("java.home")
+        val jlink = File(javaHome, "bin/jlink${if (getOs() == "windows") ".exe" else ""}")
+        require(jlink.isFile) {
+            "Cannot find 'jlink' at '${jlink.absolutePath}'. " +
+                "The configured java home ('$javaHome') must point to a JDK (not a JRE). " +
+                "Set it explicitly via 'tasks.withType<GenerateEmbeddedJreTask> { javaHome = \"/path/to/jdk\" }' " +
+                "or make sure JAVA_HOME points to a JDK."
+        }
 
         execOperations.exec { spec ->
             spec.commandLine(
-                "$resolvedJavaHome/bin/jlink",
+                jlink.absolutePath,
                 "--add-modules", modules.joinToString(","),
                 "--output", outputDir,
                 *arguments,
@@ -50,7 +60,7 @@ open class GenerateEmbeddedJreTask @Inject constructor(
         logger.lifecycle(
             "Custom JRE created in $outputDir using modules: '${modules.joinToString(",")}', arguments: '${
                 arguments.joinToString(" ")
-            }' and java home: $resolvedJavaHome"
+            }' and java home: $javaHome"
         )
     }
 
